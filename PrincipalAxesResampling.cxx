8b296d1b8b7cdea3cc640dc8bb0be7fa6f5ce364
Paul Hughett <hughett@upenn.edu>
2001-01-30 13:59:41 -0500

    ENH: Modified the CMake configuration files to compile example programs in the Examples directory; created the first example program, although it's just the canonical "Hello, world!" program for the moment.

---
+++ PrincipalAxesResampling.cxx
@@ -0,0 +1,32 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    PrincipalAxesResampling.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+
+  Copyright (c) 2000 National Library of Medicine
+  All rights reserved.
+
+  See COPYRIGHT.txt for copyright details.
+
+=========================================================================*/
+
+
+/*--------------------------------------------------------------------------
+This will eventually be a program to read an image file, find its
+principal axes, and resample the image in principal axes coordinates.
+For the moment, though, we're happy just to be able to compile it
+within Insight.
+---------------------------------------------------------------------------*/
+
+#include <stdio.h>
+
+int
+main(int argc, char *argv[])
+{
+    printf("Hello, world!\n");
+    return 0;
+}
