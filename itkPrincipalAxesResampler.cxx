7f33240f72408606d0e14b0b33b1111ac77f57da
Paul Hughett <hughett@upenn.edu>
2001-02-05 15:16:12 -0500

    ENH: Wrote all the code to resample in principal axes coordinates.  It compiles, but--alas--does not yet produce the right answers.

--- itkPrincipalAxesResampler.cxx
+++ itkPrincipalAxesResampler.cxx
@@ -16,10 +16,12 @@
 
 
 /*--------------------------------------------------------------------------
-This will eventually be a program to read an image file, find its
-principal axes, and resample the image in principal axes coordinates.
-For the moment, though, we're happy just to be able to compile it
-within Insight.
+This command line program reads an image file, finds its principal
+axes, and resamples the image into principal axes coordinates.  It
+takes two arguments: the names of the input and output image files.
+The image dimensions are fixed at 192 slices by 256 rows by 256
+columns and the data format is fixed at bigendian 16-bit unsigned
+integer.  (But see below for hooks to change these parameters.)
 ---------------------------------------------------------------------------*/
 
 #include <stdio.h>
@@ -29,41 +31,47 @@ within Insight.
 #include "itkImage.h"
 #include "itkImageMoments.h"
 #include "itkImageRegionSimpleIterator.h"
+#include "itkLinearInterpolateFunction.h"
 
 
 enum {NDimensions = 3};
 
-typedef unsigned char                       PixelType;
+typedef unsigned short                      PixelType;
 typedef itk::Image<PixelType, NDimensions>  ImageType;
 typedef ImageType::RegionType               ImageRegionType;
 typedef ImageType::SizeType                 ImageSizeType;
+typedef ImageType::AffineTransformType      AffineTransformType;
 
-typedef itk::Index<NDimensions>             ImageIndexType;
+typedef itk::Index<NDimensions>                    ImageIndexType;
 typedef itk::ImageRegionSimpleIterator<ImageType>  ImageIteratorType;
 typedef itk::ImageMoments<PixelType, NDimensions>  ImageMomentsType;
+typedef itk::LinearInterpolateFunction<ImageType>  InterpolatorType;
+typedef itk::Point<double, NDimensions>            PointType;
 
 
 int
 main(int argc, char *argv[])
 {
-    /* Tell user I'm here! */
-    printf("Hello, world!\n");
-
-    /* Set image parameters */
+    /* Set image and other parameters */
     long ImageWidth     = 256;
     long ImageHeight    = 256;
     long NumberOfSlices = 192;
+    int bigend  = 1;             // Bigendian data in external files?
+    int verbose = 1;             // Give lots of interim messages?
 
-    /* Open image file and read image */
-    /* FIXME: Translate into C++ style */
+    /* Get input and output file names from the command line */
     if (argc < 2)
         {
-            fprintf(stderr, "You must give the input file name\n");
+            fprintf(stderr, "You must give input and output file names\n");
             exit(EXIT_FAILURE);
         }
-    char *Filename = argv[1];
-    FILE *file = fopen(Filename, "rb");
-    if (file == 0) 
+    char *filename1 = argv[1];
+    char *filename2 = argv[2];
+
+    /* Open image file */
+    /* FIXME: Translate into C++ */
+    FILE *infile = fopen(filename1, "rb");
+    if (infile == 0) 
         {
             fprintf(stderr, "Unable to open input file\n");
             exit(EXIT_FAILURE);
@@ -84,9 +92,9 @@ main(int argc, char *argv[])
     /* FIXME: Find or write Insightful tools for this */
     std::cout << "Reading image file." << std::endl;
     ImageIndexType index;        // Index to current pixel
-    long point[3];               // Location of current pixel
+    unsigned long point[3];      // Location of current pixel
     unsigned char bytes[2];      // Upper and lower bytes
-    unsigned short value;
+    PixelType value;
     size_t count;
     for (long slice = 0; slice < NumberOfSlices; slice++) {
         point[2] = slice;
@@ -95,12 +103,14 @@ main(int argc, char *argv[])
             for (long col = 0; col < ImageWidth; col++) {
                 point[0] = col;
                 index.SetIndex(point);
-                count = fread(bytes, 1, 2, file);
+                count = fread(bytes, 1, 2, infile);
                 if (count != 2) {
                     fprintf(stderr, "Error reading input file\n");
                     exit(EXIT_FAILURE); }
-                // Assume little endian data
-                value = bytes[1]<<8 + bytes[0];
+                if (bigend)
+                    value = bytes[0] << 8 + bytes[1];
+                else
+                    value = bytes[1] << 8 + bytes[0];
                 image->SetPixel(index, value);
             }
         }
@@ -115,7 +125,7 @@ main(int argc, char *argv[])
     unsigned short value;
     for (iterator.Begin(); !iterator.IsAtEnd(); ++iterator)
         {
-            count = fread(bytes, 1, 2, file);
+            count = fread(bytes, 1, 2, infile);
             if (count != 2) {
                 fprintf(stderr, "Error reading input file\n");
                 exit(EXIT_FAILURE); }
@@ -126,10 +136,10 @@ main(int argc, char *argv[])
 #endif
 
     /* Close the input file */
-    fclose(file);
+    fclose(infile);
 
     /* Compute principal moments and axes */
-    std::cout << "Computing moments and principal axes." << std::endl;
+    std::cout << "Computing moments and transformation." << std::endl;
     ImageMomentsType moments(image);
     double ctm = moments.GetTotalMass();
     vnl_vector_fixed<double,3>
@@ -140,15 +150,96 @@ main(int argc, char *argv[])
         cpa = moments.GetPrincipalAxes();
 
     /* Report moments information to the user */
-    std::cout << "\nTotal mass = "        << ctm << std::endl;
-    std::cout << "\nCenter of gravity = " << ccg << std::endl;
-    std::cout << "\nPrincipal moments = " << cpm << std::endl;
-    std::cout << "\nPrincipal axes = \n";
-    std::cout << cpa << "\n";
+    if (verbose) {
+        std::cout << "\nTotal mass = "        << ctm << std::endl;
+        std::cout << "\nCenter of gravity = " << ccg << std::endl;
+        std::cout << "\nPrincipal moments = " << cpm << std::endl;
+        std::cout << "\nPrincipal axes = \n";
+        std::cout << cpa << "\n";
+    }
+
+    /* Allocate resampled image (same size as original) */
+    ImageType::Pointer resamp = ImageType::New();
+    resamp->SetLargestPossibleRegion(region);
+    resamp->SetBufferedRegion(region);
+    resamp->Allocate();
+
+    /* Compute the transform from principal axes to original axes */
+    AffineTransformType trans = resamp->GetIndexToPhysicalTransform();
+    vnl_vector_fixed<double,3> center;
+    center[0] = -ImageWidth / 2.0;
+    center[1] = -ImageHeight / 2.0;
+    center[2] = -NumberOfSlices / 2.0;
+    trans.Translate(center);
+    trans.Compose(moments.GetPrincipalAxesToPhysicalAxesTransform());
+    trans.Translate(ccg);
+    trans.Compose(image->GetPhysicalToIndexTransform());
+    if (verbose) {
+        std::cout << "Backprojection transform:" << std::endl;
+        std::cout << trans << std::endl;
+    }
+
+    /* Create and initialize the interpolator */
+    InterpolatorType::Pointer interp = InterpolatorType::New();
+    interp->SetInputImage(image);
 
     /* Resample image in principal axes coordinates */
+    std::cout << "Resampling the image" << std::endl;
+    PointType pos;                       // Position in resampled image
+    ImageIndexType ipos;                 // Ditto, but as index
+    PointType opos;                      // Position in original image
+    double    dpos[NDimensions];         // Ditto, but as double
+    for (long slice = 0; slice < NumberOfSlices; slice++) {
+        pos[2] = ipos[2] = slice;
+        for (long row = 0; row < ImageHeight; row++) {
+            pos[1] = ipos[1] = row;
+            for (long col = 0; col < ImageWidth; col++) {
+                pos[0] = ipos[0] = col;
+                opos = trans.Transform(pos);
+                for (int i = 0; i < NDimensions; ++i) {
+                    dpos[i] = opos[i];
+                }
+                value = (PixelType)(interp->Evaluate(dpos));
+                resamp->SetPixel(ipos, value);
+            }
+        }
+    }
+
+    /* Open the output file */
+    /* FIXME: Translate into C++ */
+    std::cout << "Writing the output file" << std::endl;
+    FILE *outfile = fopen(filename2, "wb");
+    if (outfile == 0) 
+        {
+            fprintf(stderr, "Unable to open output file\n");
+            exit(EXIT_FAILURE);
+        }
 
     /* Write resampled image to a file */
+    for (long slice = 0; slice < NumberOfSlices; slice++) {
+        point[2] = slice;
+        for (long row = 0; row < ImageHeight; row++) {
+            point[1] = row;
+            for (long col = 0; col < ImageWidth; col++) {
+                point[0] = col;
+                index.SetIndex(point);
+                value = resamp->GetPixel(index);
+                if (bigend) {
+                    bytes[0] = value >> 8;
+                    bytes[1] = value & 0xff;
+                }
+                else {
+                    bytes[1] = value >> 8;
+                    bytes[0] = value & 0xff;
+                }
+                count = fwrite(bytes, 1, 2, outfile);
+                if (count != 2) {
+                    fprintf(stderr, "Error reading input file\n");
+                    exit(EXIT_FAILURE); }
+            }
+        }
+    }
+    fclose(outfile);
     
     return 0;
 }
