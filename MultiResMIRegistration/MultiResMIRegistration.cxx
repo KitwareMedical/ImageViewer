70c7c5b5c94895a8c6b7fcf88a9a3448c312facd
Lydia Ng <lydiang@gmail.com>
2001-07-03 02:51:54 -0400

    ENH: new multi-resolution example using real data

--- MultiResMIRegistration/MultiResMIRegistration.cxx
+++ MultiResMIRegistration/MultiResMIRegistration.cxx
@@ -38,191 +38,34 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 =========================================================================*/
-#include "itkPhysicalImage.h"
-#include "itkImageRegionIterator.h"
-#include "itkMultiResolutionMutualInformationRigidRegistration.h"
+#include "MIRegistrationApp.h"
+#include "itkExceptionObject.h"
 
-#include <iostream>
 
-int main()
+int main(int argc, char *argv[])
 {
-
-//------------------------------------------------------------
-// Create two simple images
-// Two Gaussians with one translated (7,3,2) pixels from another
-//------------------------------------------------------------
-
-  //Allocate Images
-  typedef float PixelType;
-  typedef itk::PhysicalImage<PixelType,3>           ReferenceType;
-  typedef itk::PhysicalImage<PixelType,3>           TargetType;
-  enum { ImageDimension = ReferenceType::ImageDimension };
-
-  ReferenceType::SizeType size = {{100,100,40}};
-  ReferenceType::IndexType index = {{0,0,0}};
-  ReferenceType::RegionType region;
-  region.SetSize( size );
-  region.SetIndex( index );
-
-  ReferenceType::Pointer imgReference = ReferenceType::New();
-  imgReference->SetLargestPossibleRegion( region );
-  imgReference->SetBufferedRegion( region );
-  imgReference->SetRequestedRegion( region );
-  imgReference->Allocate();
-
-  TargetType::Pointer imgTarget = TargetType::New();
-  imgTarget->SetLargestPossibleRegion( region );
-  imgTarget->SetBufferedRegion( region );
-  imgTarget->SetRequestedRegion( region );
-  imgTarget->Allocate();
-
-  // Fill images with a 3D gaussian
-  typedef  itk::ImageRegionIterator<ReferenceType>
-    ReferenceIteratorType;
-  typedef  itk::ImageRegionIterator<TargetType>
-    TargetIteratorType;
-
-  itk::Point<double,3> center;
-  center[0] = (double)region.GetSize()[0]/2.0;
-  center[1] = (double)region.GetSize()[1]/2.0;
-  center[2] = (double)region.GetSize()[2]/2,0;
-
-  const double s = (double)region.GetSize()[0]/2.0;
-
-  itk::Point<double,3>  p;
-  itk::Vector<double,3> d;
-
-  // Set the displacement
-  itk::Vector<double,3> displacement;
-  displacement[0] = 7;
-  displacement[1] =	3;
-  displacement[2] = 2;
-
-  ReferenceIteratorType ri(imgReference,region);
-  TargetIteratorType ti(imgTarget,region);
-  ri.Begin();
-  while(!ri.IsAtEnd())
-  {
-    p[0] = ri.GetIndex()[0];
-    p[1] = ri.GetIndex()[1];
-    p[2] = ri.GetIndex()[2];
-	  d = p-center;
-	  d += displacement;
-	  const double x = d[0];
-	  const double y = d[1];
-    const double z = d[2];
-    ri.Set( (PixelType)( 200.0 * exp( - ( x*x + y*y + z*z )/(s*s) ) ) );
-    ++ri;
-  }
-
-
-  ti.Begin();
-  while(!ti.IsAtEnd())
-  {
-    p[0] = ti.GetIndex()[0];
-    p[1] = ti.GetIndex()[1];
-    p[2] = ti.GetIndex()[2];
-	d = p-center;
-	const double x = d[0];
-	const double y = d[1];
-  const double z = d[2];
-    ti.Set( (PixelType)( 200.0 * exp( - ( x*x + y*y + z*z )/(s*s) ) ) );
-    ++ti;
-  }
-
-  // set image origin to be center of the image
-  double transCenter[3];
-  for( unsigned int j = 0; j < 3; j++ )
+  if ( argc < 2 )
     {
-    transCenter[j] = -0.5 * double(size[j] - 1);
+    std::cout << "Parameter file name missing" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Usage:  MIRigidRegistration param.file" << std::endl;
+    return 1;
     }
 
-  imgReference->SetOrigin( transCenter );
-  imgTarget->SetOrigin( transCenter );
-
-
- /**
-  * Setup the registrator
-  */
-  typedef itk::MultiResolutionMutualInformationRigidRegistration<
-    ReferenceType,TargetType> MRRegistrationType;
-
-  MRRegistrationType::Pointer registrator = MRRegistrationType::New();
-
-  registrator->SetTarget( imgTarget );
-  registrator->SetReference( imgReference );
-  registrator->SetNumberOfLevels( 3 );
-
-  unsigned int niter[4] = { 300, 300, 300 };
-  double rates[4] = { 1e-6, 1e-6, 1e-7 };
-  double scales[4] = { 1000, 100, 100 };
-
-  registrator->SetNumberOfIterations( niter );
-  registrator->SetLearningRates( rates );
-  registrator->SetTranslationScales( scales );
-
-
-  MRRegistrationType::RegistrationPointer method = 
-    registrator->GetInternalRegistrationMethod();
-
-
-  // set metric related parameters
-  method->GetMetric()->SetTargetStandardDeviation( 5.0 );
-  method->GetMetric()->SetReferenceStandardDeviation( 5.0 );
-  method->GetMetric()->SetNumberOfSpatialSamples( 50 );
-
-  std::cout << "Target schedule: " << std::endl;
-  std::cout << registrator->GetTargetPyramid()->GetSchedule() << std::endl;
-
-  std::cout << "Reference schedule: " << std::endl;
-  std::cout << registrator->GetReferencePyramid()->GetSchedule() << std::endl;
-
-  /**
-   * Do the registration
-   */
-  registrator->StartRegistration();
-
-
-  /**
-   * Check the results
-   */
-  MRRegistrationType::RegistrationType::ParametersType solution = 
-    method->GetParameters();
-
-  std::cout << "Solution is: " << solution << std::endl;
-
-  //
-  // check results to see if it is within range
-  //
-  bool pass = true;
-  double trueParameters[7] = { 0, 0, 0, 1, 0, 0, 0 };
-  trueParameters[4] = - displacement[0];
-  trueParameters[5] = - displacement[1];
-  trueParameters[6] = - displacement[2];
-  for( unsigned int j = 0; j < 4; j++ )
+  // run the registration
+  try
     {
-    if( vnl_math_abs( solution[j] - trueParameters[j] ) > 0.02 )
-      {
-      pass = false;
-      }
+    MIRegistrationApp theApp( argv[1] );
+    theApp.Execute();
     }
-  for( unsigned int j = 4; j < 7; j++ )
+  catch( itk::ExceptionObject& err)
     {
-    if( vnl_math_abs( solution[j] - trueParameters[j] ) > 1.0 )
-      {
-      pass = false;
-      }
+    std::cout << "Caught an exception: " << std::endl;
+    std::cout << err.GetLocation() << std::endl;
+    std::cout << err.GetDescription() << std::endl;
     }
-
-  if( !pass )
-    {
-    std::cout << "Test failed." << std::endl;
-    return EXIT_FAILURE;
-    }
-
-  std::cout << "Test passed." << std::endl;
-  return EXIT_SUCCESS;
-
 
   return 0;
+
 }
+
