c8c79916faafa63394c21221401dd3fb54813e76
Robert Tamburo <rjtst21@pitt.edu>
2001-08-10 14:33:59 -0400

    Fixed error in comment

--- EllipsoidInteriorExteriorSpatialFunction/itkEllipsoidInteriorExteriorSpatialFunctionExample.cxx
+++ EllipsoidInteriorExteriorSpatialFunction/itkEllipsoidInteriorExteriorSpatialFunctionExample.cxx
@@ -206,7 +206,7 @@ int main()
     numInteriorPixels = numInteriorPixels1;
     }
 
-  // Volume of ellipsoid using V=(4/3)*pi*a*b*c
+  // Volume of ellipsoid using V=(4/3)*pi*(a/2)*(b/2)*(c/2)
   double volume = 4.18879013333*(axes[0]/2)*(axes[1]/2)*(axes[2]/2);
 
   // Percent difference in volume measurement and calculation.
