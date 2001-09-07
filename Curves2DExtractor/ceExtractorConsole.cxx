110ffc1569a6e1eda52614253fee7df5dc9fe798
Luis Ibanez <luis.ibanez@kitware.com>
2001-09-06 22:37:41 -0400

    ENH: Classes for extracting tubes from 2D images

---
+++ Curves2DExtractor/ceExtractorConsole.cxx
@@ -0,0 +1,536 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    ceExtractorConsole.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 2001 Insight Consortium
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * The name of the Insight Consortium, nor the names of any consortium members,
+   nor of any contributors, may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+  * Modified source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+
+
+#include <ceExtractorConsole.h>
+#include <Fl/fl_file_chooser.H>
+#include <fltkLightButtonRedrawCommand.h>
+#include <itkFileIOMetaImage.h>
+ 
+
+
+/************************************
+ *
+ *  Constructor
+ *
+ ***********************************/
+ceExtractorConsole
+::ceExtractorConsole()
+{
+ 
+  itk::FileIOMetaImageFactory * factory = new itk::FileIOMetaImageFactory;
+  itk::ObjectFactoryBase * factoryBase = static_cast<itk::ObjectFactoryBase *>( factory );
+  itk::ObjectFactoryBase::RegisterFactory( factoryBase );
+  
+  typedef fltk::LightButtonRedrawCommand  CommandButton;
+
+  m_InputViewer = InputImageViewerType::New();
+
+  m_Viewer_H1x  = ImageViewerType::New();
+  m_Viewer_H1y  = ImageViewerType::New();
+
+  m_Viewer_H2x  = ImageViewerType::New();
+  m_Viewer_H2y  = ImageViewerType::New();
+
+  m_Viewer_Laplacian = ImageViewerType::New();
+
+  m_Viewer_Gradient_Modulus = ImageViewerType::New();
+
+  m_InputViewer->SetLabel( "Input Image" );
+
+  m_Viewer_H1x->SetLabel( "Gradient X" );
+  m_Viewer_H1y->SetLabel( "Gradient Y" );
+
+  m_Viewer_H2x->SetLabel( "Second Derivative X" );
+  m_Viewer_H2y->SetLabel( "Second Derivative Y" );
+
+  m_Viewer_Laplacian->SetLabel( "Laplacian" );
+  
+  m_Viewer_Gradient_Modulus->SetLabel( "Gradient Modulus" );
+
+  fltk::ProgressBarRedrawCommand * progressUpdateCommand = 
+                            progressSlider->GetRedrawCommand().GetPointer();
+
+  m_H1x->AddObserver( itk::Command::ProgressEvent, progressUpdateCommand );
+  m_H1y->AddObserver( itk::Command::ProgressEvent, progressUpdateCommand );
+  m_H2x->AddObserver( itk::Command::ProgressEvent, progressUpdateCommand );
+  m_H2y->AddObserver( itk::Command::ProgressEvent, progressUpdateCommand );
+  m_Hx->AddObserver(  itk::Command::ProgressEvent, progressUpdateCommand );
+  m_Hy->AddObserver(  itk::Command::ProgressEvent, progressUpdateCommand );
+  m_Hxy->AddObserver(  itk::Command::ProgressEvent, progressUpdateCommand );
+  m_Add->AddObserver(  itk::Command::ProgressEvent, progressUpdateCommand );
+  m_Modulus->AddObserver(  itk::Command::ProgressEvent, progressUpdateCommand );
+                              
+  m_Reader->AddObserver( itk::Command::StartEvent, inputButton->GetRedrawCommand().GetPointer() );
+  m_Hx->AddObserver(  itk::Command::StartEvent, HxButton->GetRedrawCommand().GetPointer() );
+  m_Hy->AddObserver(  itk::Command::StartEvent, HyButton->GetRedrawCommand().GetPointer() );
+  m_Hxy->AddObserver( itk::Command::StartEvent, HxyButton->GetRedrawCommand().GetPointer() );
+  m_H1x->AddObserver( itk::Command::StartEvent, H1xButton->GetRedrawCommand().GetPointer() );
+  m_H1y->AddObserver( itk::Command::StartEvent, H1yButton->GetRedrawCommand().GetPointer() );
+  m_H2x->AddObserver( itk::Command::StartEvent, H2xButton->GetRedrawCommand().GetPointer() );
+  m_H2y->AddObserver( itk::Command::StartEvent, H2yButton->GetRedrawCommand().GetPointer() );
+  m_Add->AddObserver( itk::Command::StartEvent, laplacianButton->GetRedrawCommand().GetPointer() );
+  m_Modulus->AddObserver( itk::Command::StartEvent, modulusButton->GetRedrawCommand().GetPointer() );
+
+  m_Reader->AddObserver( itk::Command::EndEvent, inputButton->GetRedrawCommand().GetPointer() );
+  m_Hx->AddObserver(  itk::Command::EndEvent, HxButton->GetRedrawCommand().GetPointer() );
+  m_Hy->AddObserver(  itk::Command::EndEvent, HyButton->GetRedrawCommand().GetPointer() );
+  m_Hxy->AddObserver( itk::Command::EndEvent, HxyButton->GetRedrawCommand().GetPointer() );
+  m_H1x->AddObserver( itk::Command::EndEvent, H1xButton->GetRedrawCommand().GetPointer() );
+  m_H1y->AddObserver( itk::Command::EndEvent, H1yButton->GetRedrawCommand().GetPointer() );
+  m_H2x->AddObserver( itk::Command::EndEvent, H2xButton->GetRedrawCommand().GetPointer() );
+  m_H2y->AddObserver( itk::Command::EndEvent, H2yButton->GetRedrawCommand().GetPointer() );
+  m_Add->AddObserver( itk::Command::EndEvent, laplacianButton->GetRedrawCommand().GetPointer() );
+  m_Modulus->AddObserver( itk::Command::EndEvent, modulusButton->GetRedrawCommand().GetPointer() );
+
+  m_Reader->AddObserver( itk::Command::ModifiedEvent, inputButton->GetRedrawCommand().GetPointer() );
+  m_Hx->AddObserver(  itk::Command::ModifiedEvent, HxButton->GetRedrawCommand().GetPointer() );
+  m_Hy->AddObserver(  itk::Command::ModifiedEvent, HyButton->GetRedrawCommand().GetPointer() );
+  m_Hxy->AddObserver( itk::Command::ModifiedEvent, HxyButton->GetRedrawCommand().GetPointer() );
+  m_H1x->AddObserver( itk::Command::ModifiedEvent, H1xButton->GetRedrawCommand().GetPointer() );
+  m_H1y->AddObserver( itk::Command::ModifiedEvent, H1yButton->GetRedrawCommand().GetPointer() );
+  m_H2x->AddObserver( itk::Command::ModifiedEvent, H2xButton->GetRedrawCommand().GetPointer() );
+  m_H2y->AddObserver( itk::Command::ModifiedEvent, H2yButton->GetRedrawCommand().GetPointer() );
+  m_H1x->AddObserver( itk::Command::ModifiedEvent, laplacianButton->GetRedrawCommand().GetPointer() );
+  m_H1y->AddObserver( itk::Command::ModifiedEvent, laplacianButton->GetRedrawCommand().GetPointer() );
+  m_H2x->AddObserver( itk::Command::ModifiedEvent, modulusButton->GetRedrawCommand().GetPointer() );
+  m_H2y->AddObserver( itk::Command::ModifiedEvent, modulusButton->GetRedrawCommand().GetPointer() );
+
+  m_Reader->AddObserver( itk::Command::ModifiedEvent, inputButton->GetRedrawCommand().GetPointer() );
+  m_Reader->AddObserver( itk::Command::ModifiedEvent, HxButton->GetRedrawCommand().GetPointer() );
+  m_Reader->AddObserver( itk::Command::ModifiedEvent, HyButton->GetRedrawCommand().GetPointer() );
+  m_Reader->AddObserver( itk::Command::ModifiedEvent, HxyButton->GetRedrawCommand().GetPointer() );
+  m_Reader->AddObserver( itk::Command::ModifiedEvent, H1xButton->GetRedrawCommand().GetPointer() );
+  m_Reader->AddObserver( itk::Command::ModifiedEvent, H1yButton->GetRedrawCommand().GetPointer() );
+  m_Reader->AddObserver( itk::Command::ModifiedEvent, H2xButton->GetRedrawCommand().GetPointer() );
+  m_Reader->AddObserver( itk::Command::ModifiedEvent, H2yButton->GetRedrawCommand().GetPointer() );
+  m_Reader->AddObserver( itk::Command::ModifiedEvent, laplacianButton->GetRedrawCommand().GetPointer() );
+  m_Reader->AddObserver( itk::Command::ModifiedEvent, modulusButton->GetRedrawCommand().GetPointer() );
+
+  this->ShowStatus("Let's start by loading an image...");
+
+}
+
+
+
+
+/************************************
+ *
+ *  Destructor
+ *
+ ***********************************/
+ceExtractorConsole
+::~ceExtractorConsole()
+{
+
+  if( m_Viewer_H1x ) 
+  {
+    delete m_Viewer_H1x;
+    m_Viewer_H1x = 0;
+  }
+
+  if( m_Viewer_H1y ) 
+  {
+    delete m_Viewer_H1y;
+    m_Viewer_H1y = 0;
+  }
+
+  if( m_Viewer_H2x ) 
+  {
+    delete m_Viewer_H2x;
+    m_Viewer_H2x = 0;
+  }
+
+  if( m_Viewer_H2y ) 
+  {
+    delete m_Viewer_H2y;
+    m_Viewer_H2y = 0;
+  }
+
+  if( m_InputViewer ) 
+  {
+    delete m_InputViewer;
+    m_InputViewer = 0;
+  }
+
+  if( m_Viewer_Laplacian ) 
+  {
+    delete m_Viewer_Laplacian;
+    m_Viewer_Laplacian = 0;
+  }
+
+  if( m_Viewer_Gradient_Modulus ) 
+  {
+    delete m_Viewer_Gradient_Modulus;
+    m_Viewer_Gradient_Modulus = 0;
+  }
+
+
+}
+
+
+
+ 
+/************************************
+ *
+ *  Load
+ *
+ ***********************************/
+void
+ceExtractorConsole
+::Load( void )
+{
+
+  const char * filename = fl_file_chooser("Image filename","*.mh[da]","");
+  if( !filename )
+  {
+    return;
+  }
+
+  this->ShowStatus("Loading image file...");
+  
+  try 
+  {
+    ceExtractorConsoleBase::Load( filename );
+  }
+  catch( ... ) 
+  {
+    this->ShowStatus("Problems reading file format");
+    controlsGroup->deactivate();
+    return;
+  }
+
+
+  this->ShowStatus("File Loaded");
+
+  controlsGroup->activate();
+
+
+}
+
+   
+/************************************
+ *
+ *  Show
+ *
+ ***********************************/
+void
+ceExtractorConsole
+::Show( void )
+{
+  consoleWindow->show();
+}
+
+
+
+
+
+/************************************
+ *
+ *  Hide
+ *
+ ***********************************/
+void
+ceExtractorConsole
+::Hide( void )
+{
+
+  consoleWindow->hide();
+  m_Viewer_H1x->Hide();
+  m_Viewer_H1y->Hide();
+  m_Viewer_H2x->Hide();
+  m_Viewer_H2y->Hide();
+  m_InputViewer->Hide();
+  m_Viewer_Laplacian->Hide();
+  m_Viewer_Gradient_Modulus->Hide();
+}
+
+
+
+
+
+/************************************
+ *
+ *  Quit
+ *
+ ***********************************/
+void
+ceExtractorConsole
+::Quit( void )
+{
+  Hide();
+}
+
+
+
+
+ 
+/************************************
+ *
+ *  Show Status
+ *
+ ***********************************/
+void
+ceExtractorConsole
+::ShowStatus( const char * message )
+{
+  ceExtractorConsoleBase::ShowStatus( message );
+  statusTextOutput->value( message );
+  Fl::check();
+}
+
+
+
+
+ 
+/************************************
+ *
+ *  Show Input Image
+ *
+ ***********************************/
+void
+ceExtractorConsole
+::ShowInput( void )
+{
+
+  if( ! (m_ImageLoaded) ) 
+  {
+    this->ShowStatus("Please load an image first");
+    return;
+  }
+
+  m_InputViewer->SetImage( m_Reader->GetOutput() );  
+  m_InputViewer->Show();
+
+}
+
+
+
+ 
+/************************************
+ *
+ *  Show Filtered Image
+ *
+ ***********************************/
+void
+ceExtractorConsole
+::ShowFilteredX( void )
+{
+
+  m_H1x->Update();
+  m_Viewer_H1x->SetImage( m_H1x->GetOutput() );  
+  m_Viewer_H1x->Show();
+
+}
+
+
+ 
+/************************************
+ *
+ *  Show Filtered Image
+ *
+ ***********************************/
+void
+ceExtractorConsole
+::ShowFilteredY( void )
+{
+
+  m_H1y->Update(); 
+  m_Viewer_H1y->SetImage( m_H1y->GetOutput() );  
+  m_Viewer_H1y->Show();
+
+}
+
+
+
+ 
+/************************************
+ *
+ *  Show Second Derivative X
+ *
+ ***********************************/
+void
+ceExtractorConsole
+::ShowSecondDerivativeX( void )
+{
+
+  m_H2x->Update();
+  m_Viewer_H2x->SetImage( m_H2x->GetOutput() );  
+  m_Viewer_H2x->Show();
+
+}
+
+
+ 
+/************************************
+ *
+ *  Show Second Derivative Y
+ *
+ ***********************************/
+void
+ceExtractorConsole
+::ShowSecondDerivativeY( void )
+{
+
+  m_H2y->Update();
+  m_Viewer_H2y->SetImage( m_H2y->GetOutput() );  
+  m_Viewer_H2y->Show();
+
+}
+
+
+
+
+
+ 
+/************************************
+ *
+ *  Show Laplacian
+ *
+ ***********************************/
+void
+ceExtractorConsole
+::ShowLaplacian( void )
+{
+
+  m_Add->Update();
+  m_Viewer_Laplacian->SetImage( m_Add->GetOutput() );  
+  m_Viewer_Laplacian->Show();
+
+}
+
+
+
+ 
+/************************************
+ *
+ *  Show Gradient Modulus
+ *
+ ***********************************/
+void
+ceExtractorConsole
+::ShowGradientModulus( void )
+{
+
+  m_Modulus->Update();
+  m_Viewer_Gradient_Modulus->SetImage( m_Modulus->GetOutput() );  
+  m_Viewer_Gradient_Modulus->Show();
+
+}
+
+
+
+
+
+ 
+/************************************
+ *
+ *  Execute
+ *
+ ***********************************/
+void
+ceExtractorConsole
+::Execute( void )
+{
+
+  if( ! (m_ImageLoaded) ) 
+  {
+    this->ShowStatus("Please load an image first");
+    return;
+  }
+
+
+  this->ShowStatus("Filtering Image with a Gaussian...");
+
+  ceExtractorConsoleBase::Execute();
+
+
+  this->ShowStatus("Filtering done ");
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+/**
+ *
+ *   Main program 
+ *
+ */
+int main()
+{
+
+  ceExtractorConsole * console = new ceExtractorConsole();
+
+  console->Show();
+
+  Fl::run();
+
+  delete console;
+  
+  return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Curves2DExtractor/ceExtractorConsoleBase.h
@@ -0,0 +1,141 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    ceExtractorConsoleBase.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 2001 Insight Consortium
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * The name of the Insight Consortium, nor the names of any consortium members,
+   nor of any contributors, may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+  * Modified source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+
+
+
+
+#ifndef ce_FilterConsoleBase_h
+#define ce_FilterConsoleBase_h
+
+#include <itkImage.h>
+#include <itkFileIOToImageFilter.h>
+#include <itkWriteMetaImage.h>
+#include <itkRecursiveGaussianImageFilter.h>
+#include <itkFirstDerivativeRecursiveGaussianImageFilter.h>
+#include <itkSecondDerivativeRecursiveGaussianImageFilter.h>
+#include <itkAddImageFilter.h>
+#include <itkBinaryMagnitudeImageFilter.h>
+
+
+class ceExtractorConsoleBase 
+{
+
+public:
+
+  typedef   unsigned short                    InputPixelType;
+  typedef   float                             PixelType;
+  typedef   float                             ComputationType;
+
+  typedef   itk::Image< InputPixelType, 2 >   InputImageType;
+  typedef   itk::Image< PixelType, 2 >        ImageType;
+
+  typedef   itk::FileIOToImageFilter< 
+                            InputImageType >       VolumeReaderType;
+
+
+  typedef   itk::RecursiveGaussianImageFilter<
+                            InputImageType,
+                            ImageType,
+                            ComputationType  > InputGaussianFilterType;
+  
+  typedef   itk::RecursiveGaussianImageFilter<
+                            ImageType,
+                            ImageType,
+                            ComputationType  > GaussianFilterType;
+
+  typedef   itk::FirstDerivativeRecursiveGaussianImageFilter<
+                            ImageType,
+                            ImageType,
+                            ComputationType  > 
+                                GaussianFirstDerivativeFilterType;
+
+  typedef   itk::SecondDerivativeRecursiveGaussianImageFilter<
+                            ImageType,
+                            ImageType,
+                            ComputationType  > 
+                               GaussianSecondDerivativeFilterType;
+
+  typedef   itk::AddImageFilter< ImageType, 
+                            ImageType, ImageType >  AddFilterType;
+
+
+  typedef   itk::BinaryMagnitudeImageFilter< ImageType, 
+                            ImageType, ImageType >  ModulusFilterType;
+
+
+
+public:
+
+  ceExtractorConsoleBase();
+  virtual ~ceExtractorConsoleBase();
+  virtual void Load(const char * filename);
+  virtual void ShowProgress(float);
+  virtual void ShowStatus(const char * text);
+  virtual void Execute(void);
+  virtual void SetSigma( ComputationType );
+
+
+protected:
+
+  VolumeReaderType::Pointer                  m_Reader;
+
+  InputGaussianFilterType::Pointer               m_Hx;
+  InputGaussianFilterType::Pointer               m_Hy;
+
+  GaussianFilterType::Pointer                    m_Hxy;
+  GaussianFilterType::Pointer                    m_Hyz;
+
+  GaussianFirstDerivativeFilterType::Pointer     m_H1x;
+  GaussianFirstDerivativeFilterType::Pointer     m_H1y;
+
+  GaussianSecondDerivativeFilterType::Pointer    m_H2x;
+  GaussianSecondDerivativeFilterType::Pointer    m_H2y;
+
+  AddFilterType::Pointer                         m_Add;
+  ModulusFilterType::Pointer                 m_Modulus;
+
+  bool                                   m_ImageLoaded;
+
+};
+
+
+
+#endif
