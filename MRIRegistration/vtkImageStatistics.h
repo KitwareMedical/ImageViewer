7011869594c069b0c6fd6bc9412f4f7c5062bc5b
Bill Lorensen <bill.lorensen@gmail.com>
2001-09-20 07:10:13 -0400

    ERR: remove VTK_EXPORT since code is not in a dll.

--- MRIRegistration/vtkImageStatistics.h
+++ MRIRegistration/vtkImageStatistics.h
@@ -50,7 +50,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "vtkImageToImageFilter.h"
 
-class VTK_EXPORT vtkImageStatistics : public vtkImageToImageFilter
+class vtkImageStatistics : public vtkImageToImageFilter
 {
 public:
   static vtkImageStatistics *New();
