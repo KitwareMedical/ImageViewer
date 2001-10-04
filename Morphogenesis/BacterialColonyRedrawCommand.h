1ac4c6f7e350f332b9649a1d404a3d84dcb6dea6
Luis Ibanez <luis.ibanez@kitware.com>
2001-10-04 07:50:05 -0400

    Command intended to update the OpenGl drawing of the colony

---
+++ Morphogenesis/BacterialColonyRedrawCommand.h
@@ -0,0 +1,126 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    BacterialColonyRedrawCommand.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+Copyright (c) 2001 Insight Consortium
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+ * Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+
+ * Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+ * The name of the Insight Consortium, nor the names of any consortium members,
+   nor of any contributors, may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+  * Modified source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS ``AS IS''
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================*/
+
+
+#ifndef __fltk_BacterialColonyRedrawCommand_h
+#define __fltk_BacterialColonyRedrawCommand_h
+
+#include "itkCommand.h"
+#include "BacterialColony.h"
+#include "CommandEvents.h"
+
+
+namespace bio {
+
+/**
+ *  Implementation of the Command Patter for invoking OpenGL redraw
+ */
+class BacterialColonyRedrawCommand : public itk::Command
+{
+public:
+  /**
+   * Standard "Self" typedef.
+   */
+  typedef BacterialColonyRedrawCommand   Self;
+
+
+  /**
+   * Standard "Superclass" typedef.
+   */
+  typedef itk::Command  Superclass;
+
+
+  /**
+   * Smart pointer typedef support.
+   */
+  typedef itk::SmartPointer<Self>  Pointer;
+
+  /** 
+   * ConstSmart pointer typedef support.
+   */
+  typedef itk::SmartPointer<const Self>  ConstPointer;
+
+  /**
+   * Execute method will call redraw in the GL Window
+   */
+  void Execute(itk::LightObject *caller, unsigned long event);
+  void Execute(const itk::LightObject *caller, unsigned long event);
+
+
+  /** 
+   * Run-time type information (and related methods).
+   */
+  itkTypeMacro( BacterialColonyRedrawCommand, ::itk::Command );
+
+
+  /**
+   * Method for creation through the object factory.
+   */
+   itkNewMacro(Self);
+
+  
+  /**
+   * Method for setting the pointer to the class that needs
+   * to be notified
+   */
+   void SetBacterialColony( BacterialColony * colony );
+
+
+protected:
+  /**
+   * Constructor
+   */
+  BacterialColonyRedrawCommand();
+
+
+private:
+
+
+  BacterialColony::Pointer  m_BacterialColony;
+
+  
+};
+
+
+} // end namespace li
+
+
+#endif
