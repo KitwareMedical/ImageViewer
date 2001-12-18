ecdaaa6c298a4e88ceb026f4f80739a56b3c8493
Aljaz Noe <noe@grasp.cis.upenn.edu>
2001-12-18 10:00:44 -0500

    Moved MS specific #pragma's inside #ifdef's.

--- FEM/FEMRead.cxx
+++ FEM/FEMRead.cxx
@@ -39,9 +39,10 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 =========================================================================*/
 
-// disable stupid debug warnings in MSVC++
+// disable debug warnings in MS compiler
+#ifdef _MSC_VER
 #pragma warning(disable: 4786)
-#pragma warning(disable: 4503)
+#endif
 
 #include "itkFEM.h"
 #include <iostream>
