7ac16e08ac38950593e2ff803fc7e4e60bd652fd
Luis Ibanez <luis.ibanez@kitware.com>
2002-02-07 19:00:13 -0500

    Final layer of the Distance Map application where interction with the      GUI may be needed.

---
+++ DistanceMapFilter/DistanceMapFilter.cxx
@@ -0,0 +1,23 @@
+
+
+
+#include "DistanceMapFilter.h"
+#include <FL/fl_file_chooser.H>
+
+
+DistanceMapFilter::
+DistanceMapFilter()
+{
+}
+
+
+DistanceMapFilter::
+~DistanceMapFilter()
+{
+}
+
+
+
+
+  
+
---
+++ DistanceMapFilter/DistanceMapFilter.h
@@ -0,0 +1,63 @@
+
+#ifndef __DistanceMapFilter_H
+#define __DistanceMapFilter_H
+
+
+#include "DistanceMapFilterGUI.h"
+
+
+class DistanceMapFilter : public DistanceMapFilterGUI {
+
+
+public:
+  /**
+   * Standard "Self" typedef.
+   */
+  typedef DistanceMapFilter      Self;
+
+  /**
+   * Standard "Superclass" typedef.
+   */
+  typedef DistanceMapFilterGUI  Superclass;
+
+  /** 
+   * Smart pointer typedef support.
+   */
+  typedef itk::SmartPointer<Self>        Pointer;
+  typedef itk::SmartPointer<const Self>  ConstPointer;
+
+  /** 
+   * Run-time type information (and related methods).
+   */
+  itkTypeMacro(DistanceMapFilter, DistanceMapFilterGUI);
+
+  /**
+   * Method for creation through the object factory.
+   */
+  itkNewMacro(Self);  
+
+  /**
+   * Image types
+   */
+  typedef DistanceMapFilterBase::ImageType            ImageType;
+  typedef DistanceMapFilterBase::DistanceImageType    DistanceImageType;
+
+
+protected:
+
+  DistanceMapFilter();
+  virtual ~DistanceMapFilter();
+
+  DistanceMapFilter( const DistanceMapFilter & );           // not defined 
+  DistanceMapFilter operator=( const DistanceMapFilter & ); // not defined
+  
+
+};
+
+
+  
+
+
+#endif
+
+
