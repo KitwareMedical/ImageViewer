20d66b2e8a16d5cee93eb8aea3706a59a141714b
Luis Ibanez <luis.ibanez@kitware.com>
2002-03-09 19:18:14 -0500

    FIX: NumberOfClasses is now "unsigned int"

--- GaussianMinimumErrorClassifier/GaussianMinimumErrorClassifier.cxx
+++ GaussianMinimumErrorClassifier/GaussianMinimumErrorClassifier.cxx
@@ -90,7 +90,7 @@ int main(int argc, char* argv[])
   std::string classMaskFileName ;
   std::string targetFileName ;
   std::string outputFileName ;
-  int numberOfClasses ;
+  unsigned int numberOfClasses ;
   try
     {
       // get image file options
