eaee6fe7e158c2c7fc353ba510a04d25d431ae63
Luis Ibanez <luis.ibanez@kitware.com>
2002-03-18 17:30:40 -0500

    Subclass of the ThinSplinesApplicationsGUI

---
+++ ThinPlateSplines/ThinPlateSplinesApplication.cxx
@@ -0,0 +1,38 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    ThinPlateSplinesApplication.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include "ThinPlateSplinesApplication.h"
+
+
+
+ThinPlateSplinesApplication
+::ThinPlateSplinesApplication()
+{
+}
+
+
+
+
+ThinPlateSplinesApplication
+::~ThinPlateSplinesApplication()
+{
+}
+
+
+
+
+
---
+++ ThinPlateSplines/ThinPlateSplinesApplication.h
@@ -0,0 +1,53 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    ThinPlateSplinesApplication.h
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#ifndef ThinPlateSplinesApplication_H
+#define ThinPlateSplinesApplication_H
+
+
+
+#include "ThinPlateSplinesApplicationGUI.h"
+
+
+
+// This is the classe for the Application
+
+class ThinPlateSplinesApplication : public ThinPlateSplinesApplicationGUI
+{
+
+public:
+
+  typedef ThinPlateSplinesApplicationBase::PointType        PointType;
+  typedef ThinPlateSplinesApplicationBase::PointArrayType   PointArrayType;
+
+      
+public:
+
+  ThinPlateSplinesApplication();
+  ~ThinPlateSplinesApplication();
+  
+
+
+private:
+
+
+};
+
+
+
+
+#endif
