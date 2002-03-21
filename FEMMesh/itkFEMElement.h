9f1622156345af438d7b1c8a2ba0fa730ff6f9ef
Luis Ibanez <luis.ibanez@kitware.com>
2002-03-20 20:14:38 -0500

    ENH: The Element is now templated over the CellType, MeshType and number      of displacementComponentsPerPoint. That allows to avoid double inheritance.

--- FEMMesh/itkFEMElement.h
+++ FEMMesh/itkFEMElement.h
@@ -32,37 +32,33 @@ namespace fem {
 /** 
  * \brief Base class for Elements in a FEM system.
  * 
- *  Elements are associated one-to-one with Cells a pointer to 
- *  an Abstract Cell class is maintained in the Element so the
- *  geometric information can be made available to the Element.
- * 
- *  Elements also keep a fixed array to pointer to Nodes.  
- *  Every particular type of Element have a specific number of 
- *  nodes that do not change after construction.
- * 
- *  Elements are responsible for providing interpolation between
- *  the values (degrees of freedom) in the Nodes. This is done
- *  by using "Shape Functions" which are interpolating basis 
- *  functions defined in parametric coordinates.
- *
  */
-class FEMElement 
+template <    typename TCell, 
+              typename TFEMMesh, 
+              unsigned int NDisplacementComponentsPerPoint  >
+class FEMElement : public TCell
 {
 
 public:
 
-  /** Use a standard symplified type for the Node base class */
-  typedef FEMNodeBase             NodeType;
+  // this type is used to represent components of the load
+  // and the Stiffness matrix
+  typedef float       RealType;
 
-  /** Use a standard symplified type for the Cell base class */
-  typedef FEMCellBase             CellType;
+  typedef TFEMMesh                                FEMMeshType;
+  typedef typename FEMMeshType::PixelType         DisplacementType;
+  typedef typename FEMMeshType::CellTraits        CellTraits;
 
-  /**
-   * Real type used in Node and derived classes
-   * This type is used for representing the 
-   * components of the Stiffnes Matrix.
-   */
-  typedef  float      RealType;
+  
+  typedef TCell                                   CellType;
+  typedef CellType                                Superclass;
+
+  typedef typename FEMMeshType::PointsContainer     PointsContainer;
+  typedef typename FEMMeshType::PointDataContainer  PointDataContainer;
+  typedef typename FEMMeshType::CellsContainer      CellsContainer;
+  typedef typename FEMMeshType::PointIdentifier     PointIdentifier;
+  
+
 
   /** Type for representing the Stiffnes and Mass matrices  */
   typedef  vnl_matrix< RealType >       MatrixType;
@@ -70,44 +66,31 @@ public:
   /** Type for representing sets of external loads */
   typedef  vnl_vector< RealType >       LoadsVectorType;
 
-  /**
-   * Class type used in Node and derived classes to specify
-   * displacements is redefined here for easier access.
-   */
-   typedef NodeType::DisplacementType  DisplacementType;
+  /**  Number of components of the displacemente field at one point */
+  enum { NumberOfDisplacementComponentsPerPoint = NDisplacementComponentsPerPoint };
 
   /**
    * Return the number of components of the Displacement field
    * also known as degrees of freedom (DOF) for a derived element class
    */
-  virtual unsigned int GetNumberOfDisplacementComponents( void ) const = 0;
-
-  /**
-   * Return the number of Nodes used by this Element
-   */
-  virtual unsigned int GetNumberOfNodes( void ) const = 0;
+  unsigned int GetNumberOfDisplacementComponents( void ) 
+    { return this->GetNumberOfPoints() * NumberOfDisplacementComponentsPerPoint; }
 
 
   /**
-   *
-   * The memory address of the Displacement component is used as an Identifier in the global
-   * FEM mesh. This Identifier allows to align the contribution of equations comming from every
-   * Element in the system.
-   *
-   * Number of different pointers in a whole system determines global number of displacement
-   * components. If two pointers in two different elements point to the same location, this 
-   *  means that those two elements share that displacement component and are connected together.
-   *
-   * Normally this function is overriden by defining a simple switch statement that returns
-   * const references to members in nodes object that define the element. If an error occurs
-   * (when i is out of range for example), an ExceptionObject is thrown.
+   *  The Cell return the displacement associated with the ith point
+   *  of its list of points
    */
-  virtual const DisplacementType & GetDisplacementComponent(unsigned int i) const = 0;
+  const DisplacementType & 
+     GetDisplacement(unsigned int i,const FEMMeshType *mesh ) 
+      { const PointIdentifier Id = *( this->GetPointIds() + i );
+        return mesh->GetPointData()->ElementAt( Id ); }
+
 
   /**
    * Compute and return element stiffnes matrix in global coordinate system
    */
-  virtual MatrixType GetStiffnessMatrix( void ) const = 0;
+  virtual MatrixType GetStiffnessMatrix( const FEMMeshType * ) const = 0;
 
   /**
    * Compute and return element mass matrix in global coordinate system.
@@ -117,22 +100,6 @@ public:
   virtual MatrixType GetMassMatrix( void ) const 
     { return MatrixType(); }
 
-  /** Set the pointer to the Cell associated with this Element.
-   *  Cells represent the geometry of the object while Element represent
-   *  the distribition of the Displacement Field over the object      */
-  void SetCell( const CellType * cell )
-           { m_Cell = cell; }
-
-  /** Get the pointer to the Cell associated with this Element.
-   *  Cells represent the geometry of the object while Element represent
-   *  the distribition of the Displacement Field over the object          */
-  const CellType * GetCell( void ) const
-           { return m_Cell; }
-
-
-private:
-
-   const CellType  *   m_Cell;
 
 
 };
