2eb535c9d2db040f4e3ccc6f75c200e8df5a1360
Luis Ibanez <luis.ibanez@kitware.com>
2002-03-29 10:06:16 -0500

    ENH: fixed a debuggin message.

--- FEMMesh/itkFEMElementQuadrilateralAreaVisitor.h
+++ FEMMesh/itkFEMElementQuadrilateralAreaVisitor.h
@@ -64,7 +64,7 @@ public:
     std::cout << "p0 = " << p0 << std::endl;
     std::cout << "p1 = " << p1 << std::endl;
     std::cout << "p2 = " << p2 << std::endl;
-    std::cout << "p3 = " << p2 << std::endl;
+    std::cout << "p3 = " << p3 << std::endl;
     const double area = ( p1[0] * p2[1] - p1[1] * p2[0] + 
                           p2[0] * p0[1] - p2[1] * p0[0] +
                           p0[0] * p1[1] - p0[1] * p1[0] +
