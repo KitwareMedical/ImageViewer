34ec22fca16247c144f093368d1e144884fa2cf8
Bill Lorensen <bill.lorensen@gmail.com>
2002-06-01 12:32:56 -0400

    ENH: first try.

--- vtkITK/Scripts/TestMutualInformation.tcl
+++ vtkITK/Scripts/TestMutualInformation.tcl
@@ -18,27 +18,24 @@ set DEF_VV_VAR 2.0
 set DEF_ROT_STEP 0.005
 set DEF_TRA_STEP 100
 
-set GEVTK_DATA_ROOT /projects/vtk/GEVTKproduction/GEVTKData
+set GEVTK_DATA_ROOT c:/lorensen/GEVTKData
 #
 # Read in the test data.
 #
 vtkImageReader reader
      reader SetDataByteOrderToLittleEndian
+     reader SetDataScalarTypeToUnsignedShort
      reader SetDataSpacing 0.8  0.8  1.5
-     reader SetDataExtent 0 255 0 255 1 94
+     reader SetDataExtent 0 255 0 255 1 10
      reader SetFilePrefix "$GEVTK_DATA_ROOT/Data/fullHead/headsq"
      reader SetDataMask 0x7fff
-     reader UpdateInformation
-     reader Update
- 
-set bounds [[reader GetOutput] GetBounds]
-reader SetDataOrigin [expr {-1*([lindex $bounds 0]+[lindex $bounds 1])/2.0}] \
-    [expr {-1*([lindex $bounds 2]+[lindex $bounds 3])/2.0}] \
-    [expr {-1*([lindex $bounds 4]+[lindex $bounds 5])/2.0}]
-set extent [[reader GetOutput] GetWholeExtent]
 
-set LowerZSlice [lindex $extent 4]
-set UpperZSlice [lindex $extent 5]
+vtkImageCast castToFloat
+ castToFloat SetOutputScalarTypeToFloat
+ castToFloat SetInput [reader GetOutput]
+
+set LowerZSlice 1
+set UpperZSlice 94
 set DefaultZSlice [expr {round(($LowerZSlice+$UpperZSlice)/2.0)}]
 
 vtkTransform trans
@@ -48,38 +45,58 @@ vtkTransform trans
 
 vtkImageReslice slicer
     slicer SetInterpolationModeToCubic
-    slicer SetInput [reader GetOutput]
+    slicer SetInput [castToFloat GetOutput]
     slicer SetBackgroundLevel 0
     slicer SetResliceTransform trans
     slicer Update
 
+vtkImageChangeInformation changeSource
+  changeSource SetInput [slicer GetOutput]
+  changeSource CenterImageOn
+
+vtkITKNormalizeImageFilter normalizeSource
+    normalizeSource SetInput [changeSource GetOutput]
+
+vtkImageShrink3D shrinkSource
+  shrinkSource SetInput [normalizeSource GetOutput]
+  shrinkSource SetShrinkFactors 4 4 1
+
+vtkImageChangeInformation changeTarget
+  changeTarget SetInput [castToFloat GetOutput]
+  changeTarget CenterImageOn
+
+vtkITKNormalizeImageFilter normalizeTarget
+    normalizeTarget SetInput [changeTarget GetOutput]
+
+vtkImageShrink3D shrinkTarget
+  shrinkTarget SetShrinkFactors 4 4 1
+  shrinkTarget SetInput [normalizeTarget GetOutput]
 
 vtkITKMutualInformationTransform mutual
-    mutual SetSourceImage [reader GetOutput]
-    mutual SetTargetImage [slicer GetOutput]
-    mutual SetNumberOfIterations 500
+    mutual SetSourceImage [shrinkSource GetOutput]
+    mutual SetTargetImage [shrinkTarget GetOutput]
+    mutual SetNumberOfIterations 200
     mutual SetNumberOfSamples 50
     mutual SetLearningRate .001
     mutual SetTranslateScale 64
 
-
 vtkImageReslice newSlicer
     newSlicer SetInterpolationModeToCubic
-    newSlicer SetInput [reader GetOutput]
+    newSlicer SetInput [changeSource GetOutput]
     newSlicer SetResliceTransform mutual
     newSlicer SetBackgroundLevel 0
 
-set colorLevel 1000
-set colorWindow 2000
+set colorLevel 100
+set colorWindow 1
 
 vtkImageCheckerboard checkers
-    checkers SetInput 0 [slicer GetOutput]
-    checkers SetInput 1 [newSlicer GetOutput]
+    checkers SetInput 0 [reader GetOutput]
+    checkers SetInput 1 [reader GetOutput]
     checkers SetNumberOfDivisions 5 5 5
 
 vtkImageMapper imageMapperSource
-    imageMapperSource SetInput [checkers GetOutput]
-    imageMapperSource SetZSlice $DefaultZSlice
+    imageMapperSource SetInput [reader GetOutput]
+    imageMapperSource SetZSlice 10
     imageMapperSource SetColorWindow $colorWindow
     imageMapperSource SetColorLevel $colorLevel
 
@@ -91,34 +108,13 @@ vtkActor2D actor1
 ren1 AddActor actor1
 #ren1 SetBackground 0.8 0.8 0.8
 
-renWin SetSize [lindex $bounds 1] [lindex $bounds 3]
-renWin Render
+renWin SetSize 256 256
 
-if { [info command rtExMath] != ""} {
-  set numIters 10
-} else {
-  set numIters 250
-
-}
-for {set iters 0} {$iters<3} {incr iters} {
-    mutual SetNumberOfIterations $numIters
-    for {set ctr 0} {$ctr<1} {incr ctr} {
-         mutual Modified
-         mutual Update
-         renWin Render
-     }
- }
 
 #
 # Render the image
 iren SetUserMethod {wm deiconify .vtkInteract}
 iren Initialize
-
+renWin Render
 # Prevent the tk window from showing up then start the event loop
 wm withdraw .
-
-
-
-
-
-
