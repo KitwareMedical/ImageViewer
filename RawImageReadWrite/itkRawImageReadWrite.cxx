0a84e7d909a1d5efbe21be55ee2207450e12585b
Luis Ibanez <luis.ibanez@kitware.com>
2002-06-04 16:12:29 -0400

    Example of writing and reading Raw image files.

---
+++ RawImageReadWrite/itkRawImageReadWrite.cxx
@@ -0,0 +1,125 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    itkRawImageReadWrite.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+
+#include "itkImage.h"
+
+// Include generic Input Source
+#include "itkImageFileReader.h"
+
+// Incluide generic Output Sink
+#include "itkImageFileWriter.h"
+
+// Include file-format specific readers/writers
+#include "itkRawImageIO.h"
+
+
+
+int main(int argc, char **argv)
+{
+
+  // Taking parameters from the command line
+
+  if( argc < 6 ) 
+    {
+    std::cerr << std::endl;
+    std::cerr << "Usage:  RawImageReadWrite  inputImageFile.raw   sizeX   sizeY   sizeZ   outputImageFile.raw" << std::endl;
+    std::cerr << std::endl;
+    return -1;
+    }
+
+  const char * inputFileName  = argv[1];
+  const char * outputFileName = argv[5];
+
+  const unsigned int nx = atoi( argv[2] );
+  const unsigned int ny = atoi( argv[3] );
+  const unsigned int nz = atoi( argv[4] );
+
+
+  typedef  unsigned char    PixelType;
+  const    unsigned int     Dimension = 3;
+
+  typedef  itk::Image< PixelType, Dimension >     ImageType;
+
+
+
+  // Read a Raw File
+  typedef  itk::ImageFileReader< ImageType >      FileSourceType;
+  typedef  itk::RawImageIO<PixelType,Dimension>   RawReaderType;
+
+
+  FileSourceType::Pointer fileSource = FileSourceType::New();
+  fileSource->SetFileName( inputFileName );
+
+  RawReaderType::Pointer  rawReader  = RawReaderType::New();
+  rawReader->SetDimensions( 0, nx );
+  rawReader->SetDimensions( 1, ny );
+  rawReader->SetDimensions( 2, nz );
+
+  fileSource->SetImageIO( rawReader );
+
+  try
+    {
+    fileSource->Update();
+    }
+  catch( itk::ExceptionObject & e )
+    {
+    std::cerr << "Exception caught during Raw file reading " << std::endl;
+    std::cerr << e << std::endl;
+    return -1;
+    }
+  
+  std::cout << "File succesfully read ! " << std::endl;
+
+  // Print information about the image 
+  ImageType::Pointer  image = fileSource->GetOutput();
+  image->Print( std::cout );
+  
+
+
+  // Write a Raw File
+  typedef  itk::ImageFileWriter< ImageType >      FileSinkType;
+  typedef  itk::RawImageIO<PixelType,Dimension>   RawWriterType;
+
+
+  FileSinkType::Pointer   fileSink   = FileSinkType::New();
+  RawWriterType::Pointer  rawWriter  = RawWriterType::New();
+
+  fileSink->SetImageIO( rawWriter );
+  fileSink->SetFileName( outputFileName );
+  fileSink->SetInput( image );
+
+  
+  try
+    {
+    fileSink->Write();
+    }
+  catch( itk::ExceptionObject & e )
+    {
+    std::cerr << "Exception caught during Raw file writing " << std::endl;
+    std::cerr << e << std::endl;
+    return -1;
+    }
+
+  std::cout << "File succesfully writen ! " << std::endl;
+
+  return 0;
+
+}
+
+
+
