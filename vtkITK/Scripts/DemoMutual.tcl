4d9b739d4a592926310df65628184629aafdeab4
Bill Lorensen <bill.lorensen@gmail.com>
2002-06-11 23:16:08 -0400

    ENH: First working version.

--- vtkITK/Scripts/DemoMutual.tcl
+++ vtkITK/Scripts/DemoMutual.tcl
@@ -25,6 +25,7 @@ set DEF_TRA_STEP 100
 
 set GEVTK_DATA_ROOT /projects/vtk/GEVTKproduction/GEVTKData
 set GEVTK_DATA_ROOT c:/lorensen/GEVTKData
+
 #
 # Read in the test data.
 #
@@ -32,21 +33,32 @@ vtkImageReader reader
      reader SetDataByteOrderToLittleEndian
      reader SetDataScalarTypeToUnsignedShort
      reader SetDataSpacing 0.8  0.8  1.5
-     reader SetDataExtent 0 255 0 255 1 94
+     reader SetDataExtent 0 255 0 255 1 93
      reader SetFilePrefix "$GEVTK_DATA_ROOT/Data/fullHead/headsq"
      reader SetDataMask 0x7fff
 
+vtkImageChangeInformation center
+  center SetInput [reader GetOutput]
+  center CenterImageOn
+
+set mag 2
+vtkImageShrink3D shrink
+  shrink SetShrinkFactors $mag $mag 1
+  shrink SetInput [center GetOutput]
+  shrink AveragingOn
+
 vtkImageCast castToFloat
  castToFloat SetOutputScalarTypeToFloat
- castToFloat SetInput [reader GetOutput]
+ castToFloat SetInput [shrink GetOutput]
 
 set LowerZSlice 1
-set UpperZSlice 94
+set UpperZSlice 93
 set DefaultZSlice [expr {round(($LowerZSlice+$UpperZSlice)/2.0)}]
 
 vtkTransform trans
-#    trans Translate -5 -5 5
-    trans RotateX 10
+    trans RotateZ 10
+    trans RotateY 5
+    trans Translate -15 5 10
     trans Update
 
 vtkImageReslice slicer
@@ -54,16 +66,15 @@ vtkImageReslice slicer
     slicer SetInput [castToFloat GetOutput]
     slicer SetBackgroundLevel 0
     slicer SetResliceTransform trans
-    slicer Update
 
 vtkImageChangeInformation changeSource
   changeSource SetInput [slicer GetOutput]
   changeSource CenterImageOn
-  changeSource Update
 
 vtkImageShrink3D shrinkSource
   shrinkSource SetInput [changeSource GetOutput]
   shrinkSource SetShrinkFactors 4 4 1
+  shrinkSource AveragingOn
 
 vtkITKNormalizeImageFilter normalizeSource
     normalizeSource SetInput [shrinkSource GetOutput]
@@ -75,9 +86,11 @@ vtkImageChangeInformation changeTarget
 vtkImageShrink3D shrinkTarget
   shrinkTarget SetShrinkFactors 4 4 1
   shrinkTarget SetInput [changeTarget GetOutput]
+  shrinkTarget AveragingOff
 
 vtkITKNormalizeImageFilter normalizeTarget
     normalizeTarget SetInput [shrinkTarget GetOutput]
+    normalizeTarget Update
 
 vtkITKMutualInformationTransform mutual
     mutual SetSourceImage [normalizeSource GetOutput]
@@ -96,18 +109,32 @@ vtkImageReslice newSlicer
 set colorLevel 1000
 set colorWindow 2000
 
+vtkMarchingContourFilter marchTarget
+  marchTarget SetInput [changeTarget GetOutput]
+  marchTarget SetValue 0 600
+ 
 vtkMarchingContourFilter marchSource
   marchSource SetInput [changeSource GetOutput]
   marchSource SetValue 0 600
  
+vtkTransformPolyDataFilter transformSource
+  transformSource SetTransform mutual
+  transformSource SetInput [marchSource GetOutput]
+
 vtkImageCheckerboard checkers
     checkers SetInput 0 [changeTarget GetOutput]
     checkers SetInput 1 [newSlicer GetOutput]
     checkers SetNumberOfDivisions 2 2 1
 
+set mag 4
+vtkImageMagnify magCheckers
+  magCheckers SetInput [checkers GetOutput]
+  magCheckers SetMagnificationFactors $mag $mag 1
+  magCheckers InterpolateOn
+
 vtkImageMapper imageMapperSource
-    imageMapperSource SetInput [checkers GetOutput]
-    imageMapperSource SetZSlice 45
+    imageMapperSource SetInput [magCheckers GetOutput]
+    imageMapperSource SetZSlice 20
     imageMapperSource SetColorWindow $colorWindow
     imageMapperSource SetColorLevel $colorLevel
 
@@ -118,18 +145,27 @@ vtkPolyDataMapper sourceMapper
   sourceMapper SetInput [marchSource GetOutput]
   sourceMapper ScalarVisibilityOff
 
-vtkActor actorSource
+vtkLODActor actorSource
   actorSource SetMapper sourceMapper
+eval   [actorSource GetProperty] SetDiffuseColor .2 .8 .3
+
+vtkPolyDataMapper targetMapper
+  targetMapper SetInput [marchTarget GetOutput]
+  targetMapper ScalarVisibilityOff
+
+vtkActor actorTarget
+  actorTarget SetMapper targetMapper
 
 #
 # Add the actor to the renderer, set the background and size
 ren1 AddActor actor1
 ren2 AddActor actorSource
+ren2 AddActor actorTarget
 
 #ren1 SetBackground 0.8 0.8 0.8
 
-renWin SetSize 256 256
-renWin Render
+renWin SetSize 512 512
+renWin2 SetSize 512 512
 
 #
 # Render the image
@@ -137,11 +173,18 @@ iren SetUserMethod {wm deiconify .vtkInteract}
 iren Initialize
 iren2 Initialize
 
-renWin Render
 renWin2 Render
+renWin Render
+
 # Prevent the tk window from showing up then start the event loop
 wm withdraw .
 vtkTransform a
+vtkTransform b
+proc slice {n} {
+  imageMapperSource SetZSlice $n
+  renWin Render
+}
+
 proc go {m n} {
   shrinkSource SetShrinkFactors $n $n 1
   shrinkSource SetShrinkFactors $n $n 1
@@ -152,8 +195,13 @@ proc go {m n} {
     mutual SetLearningRate $l
     for {set i 0} { $i < $m } {incr i} {
     mutual Modified
+    eval b SetMatrix [mutual GetMatrix]
+    b Inverse
+    transformSource SetTransform b
+        actorSource SetUserMatrix [b GetMatrix]
     renWin Render
-    eval a SetMatrix [mutual GetMatrix]
-        puts "[a GetOrientation]"
+    renWin2 Render
+        eval a SetMatrix [mutual GetMatrix]
+        puts "[a GetOrientation] [a GetPosition]"
     }
 }
