fdc6812b37d8f49951b381c51468f2ef795892c9
Luis Ibanez <luis.ibanez@kitware.com>
2002-09-12 11:53:12 -0400

    ENH: ITK and Qt declarations combined.

--- QtITK/qtITK.cxx
+++ QtITK/qtITK.cxx
@@ -2,12 +2,41 @@
 #include <qapplication.h>
 #include <qpushbutton.h>
 
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkDiscreteGaussianImageFilter.h"
+
 int main(int argc, char **argv)
 {
+
+  typedef itk::Image< float, 2 > ImageType;
+
+  typedef itk::DiscreteGaussianImageFilter< 
+                    ImageType,
+                    ImageType >  FilterType;
+
+  typedef itk::ImageFileReader< ImageType > ReaderType;
+
+  ReaderType::Pointer reader = ReaderType::New();
+  
+  FilterType::Pointer filter = FilterType::New();
+
+  filter->SetInput( reader->GetOutput() );
+
+  // Create Qt Application to let Qt get its 
+  // parameters from the command line
   QApplication app( argc, argv );
-  QPushButton  bb( "Insight + Qt", 0 );
+
+  reader->SetFileName( argv[1] );
+
+  QPushButton  bb( "Start", 0 );
   bb.resize( 100, 30 );
+
+//  QObject::connect( &bb, SIGNAL(clicked()), &a, SLOT(quit()) );
+
   app.setMainWidget( &bb );
   bb.show();
+
   return app.exec();
+
 }
