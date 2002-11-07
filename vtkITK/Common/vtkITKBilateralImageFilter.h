b7c57cea9ee54e1f4a4b27fcc5fff46bf8a57d9b
Daniel Blezek <blezek.daniel@mayo.edu>
2002-11-07 08:52:24 -0500

    ENH: Bit of code cleanup

--- vtkITK/Common/vtkITKBilateralImageFilter.h
+++ vtkITK/Common/vtkITKBilateralImageFilter.h
@@ -26,19 +26,15 @@ class VTK_EXPORT vtkITKBilateralImageFilter : public vtkITKImageToImageFilterFF
   vtkTypeRevisionMacro(vtkITKBilateralImageFilter, vtkITKImageToImageFilterFF);
 
   void SetDomainSigma ( double v1, double v2, double v3)
-    {
+  {
     double v[3];
     v[0] = v1; v[1] = v2; v[2] = v3;
-    this->GetImageFilterPointer()->SetDomainSigma (v);
-    }
+    DelegateITKInputMacro ( SetDomainSigma, v );
+  }
   void SetRangeSigma ( double v1 )
-    {
-    this->GetImageFilterPointer()->SetRangeSigma (v1);
-    }
-  void SetFilterDimensionality ( unsigned int m)
-    {
-    this->GetImageFilterPointer()->SetFilterDimensionality (m);
-    }
+  {
+    DelegateITKInputMacro(SetRangeSigma, v1 );
+  }
 
 protected:
   //BTX
@@ -54,7 +50,7 @@ private:
   void operator=(const vtkITKBilateralImageFilter&);  // Not implemented.
 };
 
-vtkCxxRevisionMacro(vtkITKBilateralImageFilter, "$Revision: 1.1 $");
+vtkCxxRevisionMacro(vtkITKBilateralImageFilter, "$Revision: 1.2 $");
 vtkStandardNewMacro(vtkITKBilateralImageFilter);
 
 #endif
