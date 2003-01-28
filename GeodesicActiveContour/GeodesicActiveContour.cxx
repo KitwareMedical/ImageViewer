967342f088b051948ca356ce0480230d7dc6e8b1
Luis Ibanez <luis.ibanez@kitware.com>
2003-01-28 18:55:48 -0500

    Demo application illustrating the use of the GeodesicActiveContour filter.

---
+++ GeodesicActiveContour/GeodesicActiveContour.cxx
@@ -0,0 +1,467 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    GeodesicActiveContour.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include <GeodesicActiveContour.h>
+#include <FL/fl_file_chooser.H>
+
+
+
+
+
+/************************************
+ *
+ *  Constructor
+ *
+ ***********************************/
+GeodesicActiveContour
+::GeodesicActiveContour()
+{
+
+  m_InputImageViewer.SetLabel("Input Image");
+
+  m_ThresholdedImageViewer.SetLabel("Thresholded Image");
+
+  m_ZeroSetImageViewer.SetLabel("Fast Marching Output");
+
+  m_FastMarchingImageViewer.SetLabel("Zero Set Image");
+
+  m_GradientMagnitudeImageViewer.SetLabel("Gradient Magnitude Image");
+
+  m_EdgePotentialImageViewer.SetLabel("Edge Potential Image");
+
+  m_InputImageViewer.ClickSelectCallBack( ClickSelectCallback, (void *)this);
+
+  // Initialize ITK filter with GUI values
+  m_SigmoidFilter->SetAlpha( sigmoidAlphaValueInput->value() );
+  m_SigmoidFilter->SetBeta(  sigmoidBetaValueInput->value()  );
+
+  this->SetZeroSetValue( zeroSetValueInput->value() );
+
+  m_GeodesicActiveContourFilter->SetNumberOfIterations( 
+        static_cast<unsigned int>( geodesicActiveContourIterationsValueInput->value() ) );
+
+  m_GeodesicActiveContourFilter->SetNarrowBandwidth( 
+               geodesicActiveContourIterationsValueInput->value() );
+
+  m_GeodesicActiveContourFilter->SetNarrowBanding( 
+       static_cast<int>( geodesicActiveContourNarrowBandingCheckButton->value() ) );
+
+  m_GeodesicActiveContourFilter->SetInflationStrength( 
+       static_cast<int>( geodesicActiveContourInflationStrengthValueInput->value() ) );
+
+  m_DerivativeFilter->SetSigma( sigmaValueInput->value() );
+
+  m_FastMarchingFilter->SetStoppingValue( fastMarchingStoppingValueInput->value() );
+
+  m_VTKSegmentedImageViewer = VTKImageViewerType::New();
+  m_VTKSegmentedImageViewer->SetImage( m_ThresholdFilter->GetOutput() );
+
+  m_OutputLevelSetViewer.SetLabel("Output Level Set");
+
+  // Connect Observers in the GUI 
+  inputImageButton->Observe( m_ImageReader.GetPointer() );
+  thresholdedImageButton->Observe( m_ThresholdFilter.GetPointer() );
+  outputLevelSetButton->Observe( m_GeodesicActiveContourFilter.GetPointer() );
+  gradientMagnitudeButton->Observe( m_DerivativeFilter.GetPointer() );
+  edgePotentialButton->Observe( m_SigmoidFilter.GetPointer() );
+  fastMarchingResultButton->Observe( m_FastMarchingFilter.GetPointer() );
+
+  progressSlider->Observe( m_CastImageFilter.GetPointer() );
+  progressSlider->Observe( m_DerivativeFilter.GetPointer() );
+  progressSlider->Observe( m_ThresholdFilter.GetPointer() );
+  progressSlider->Observe( m_SigmoidFilter.GetPointer() );
+  progressSlider->Observe( m_ImageReader.GetPointer() );
+  progressSlider->Observe( m_GeodesicActiveContourFilter.GetPointer() );
+  progressSlider->Observe( m_FastMarchingFilter.GetPointer() );
+  progressSlider->Observe( m_InputThresholdFilter.GetPointer() );
+  
+
+}
+
+
+
+/************************************
+ *
+ *  Destructor
+ *
+ ***********************************/
+GeodesicActiveContour
+::~GeodesicActiveContour()
+{
+
+}
+
+
+
+
+/************************************
+ *
+ * Show main console
+ *
+ ***********************************/
+void
+GeodesicActiveContour
+::ShowConsole(void)
+{
+  consoleWindow->show();
+}
+
+
+
+
+/********************************************
+ *
+ * Quit : requires to hide all fltk windows
+ *
+ *******************************************/
+void
+GeodesicActiveContour
+::Quit(void)
+{
+  m_InputImageViewer.Hide();
+  m_ThresholdedImageViewer.Hide();
+  m_EdgePotentialImageViewer.Hide();
+  m_GradientMagnitudeImageViewer.Hide();
+  m_OutputLevelSetViewer.Hide();
+  m_ZeroSetImageViewer.Hide();
+  m_FastMarchingImageViewer.Hide();
+  
+  m_VTKSegmentedImageViewer->Hide();
+
+  consoleWindow->hide();
+}
+
+
+
+
+
+
+ 
+/************************************
+ *
+ *  Load Input Image
+ *
+ ***********************************/
+void
+GeodesicActiveContour
+::LoadInputImage( void )
+{
+
+  const char * filename = fl_file_chooser("Input Image filename","*.*","");
+  if( !filename )
+  {
+    return;
+  }
+
+  this->ShowStatus("Loading input image file...");
+  
+  try 
+  {
+    GeodesicActiveContourBase::LoadInputImage( filename );
+  }
+  catch( ... ) 
+  {
+    this->ShowStatus("Problems reading file format");
+    controlsGroup->deactivate();
+    return;
+  }
+
+  this->ShowStatus("Input Image Loaded");
+
+  controlsGroup->activate();
+
+}
+
+
+
+ 
+/************************************
+ *
+ *  Show Status
+ *
+ ***********************************/
+void
+GeodesicActiveContour
+::ShowStatus( const char * message )
+{
+  statusTextOutput->value( message );
+  Fl::check();
+}
+
+
+
+
+ 
+/************************************
+ *
+ *  Show Input Image
+ *
+ ***********************************/
+void
+GeodesicActiveContour
+::ShowInputImage( void )
+{
+
+  if( !m_InputImageIsLoaded )
+    {
+    return;
+    }
+
+  m_CastImageFilter->Update();
+  m_InputImageViewer.SetImage( m_CastImageFilter->GetOutput() );  
+  m_InputImageViewer.SetOverlay( m_SeedImage );
+  m_InputImageViewer.Show();
+
+}
+
+
+
+
+ 
+/************************************
+ *
+ *  Show Time Crossing Map Image
+ *
+ ***********************************/
+void
+GeodesicActiveContour
+::ShowOutputLevelSet( void )
+{
+
+  if( !m_InputImageIsLoaded )
+    {
+    return;
+    }
+  this->RunGeodesicActiveContour();
+  m_OutputLevelSetViewer.SetImage( m_GeodesicActiveContourFilter->GetOutput() );  
+  m_OutputLevelSetViewer.Show();
+
+}
+
+
+  
+
+
+/************************************
+ *
+ *  Show Gradient Magnitude
+ *
+ ***********************************/
+void
+GeodesicActiveContour
+::ShowGradientMagnitudeImage( void )
+{
+
+  if( !m_InputImageIsLoaded )
+    {
+    return;
+    }
+  this->ComputeGradientMagnitude();
+  m_GradientMagnitudeImageViewer.SetImage( m_DerivativeFilter->GetOutput() );  
+  m_GradientMagnitudeImageViewer.Show();
+
+}
+
+
+
+ 
+/************************************
+ *
+ *  Show The Edge Potential Map
+ *
+ ***********************************/
+void
+GeodesicActiveContour
+::ShowEdgePotentialImage( void )
+{
+
+  if( !m_InputImageIsLoaded )
+    {
+    return;
+    }
+  this->ComputeEdgePotential();
+  m_EdgePotentialImageViewer.SetImage( m_SigmoidFilter->GetOutput() );  
+  m_EdgePotentialImageViewer.Show();
+
+}
+
+
+
+
+
+ 
+/************************************
+ *
+ *  Show Thresholded Image
+ *
+ ***********************************/
+void
+GeodesicActiveContour
+::ShowThresholdedImage( void )
+{
+  m_CastImageFilter->Update();
+  m_ThresholdFilter->Update();
+  m_ThresholdedImageViewer.SetImage( m_CastImageFilter->GetOutput() );
+  m_ThresholdedImageViewer.SetOverlay( m_ThresholdFilter->GetOutput() );  
+  m_ThresholdedImageViewer.Show();
+  m_ThresholdedImageViewer.SetOverlayOpacity( 0.5 );
+
+}
+
+
+
+
+ 
+/************************************
+ *
+ *  Show Fast Marching Result Image
+ *
+ ***********************************/
+void
+GeodesicActiveContour
+::ShowFastMarchingResultImage( void )
+{
+  m_CastImageFilter->Update();
+  m_InputThresholdFilter->Update();
+  m_FastMarchingImageViewer.SetImage( m_CastImageFilter->GetOutput() );
+  m_FastMarchingImageViewer.SetOverlay( m_InputThresholdFilter->GetOutput() );
+  m_FastMarchingImageViewer.Show();
+  m_FastMarchingImageViewer.SetOverlayOpacity( 0.5 );
+}
+
+
+
+
+
+
+
+ 
+/************************************
+ *
+ *  Show Homogeneous Image
+ *
+ ***********************************/
+void
+GeodesicActiveContour
+::ShowThresholdedImageWithVTK( void )
+{
+  m_VTKSegmentedImageViewer->Show();
+}
+
+
+
+
+ 
+/*****************************************
+ *
+ *  Callback for Selecting a seed point
+ *
+ *****************************************/
+void
+GeodesicActiveContour
+::ClickSelectCallback(float x, float y, float z, float value, void * args )
+{
+
+  GeodesicActiveContour * self = 
+     static_cast<GeodesicActiveContour *>( args );
+
+  self->SelectSeedPoint( x, y, z );
+
+}
+
+
+
+ 
+/*****************************************
+ *
+ *  Callback for Selecting a seed point
+ *
+ *****************************************/
+void
+GeodesicActiveContour
+::SelectSeedPoint(float x, float y, float z)
+{
+
+  typedef SeedImageType::IndexType IndexType;
+  IndexType seed;
+  seed[0] = static_cast<IndexType::IndexValueType>( x );
+  seed[1] = static_cast<IndexType::IndexValueType>( y );
+  seed[2] = static_cast<IndexType::IndexValueType>( z );
+
+  GeodesicActiveContourBase::AddSeed( seed );
+
+  m_InputImageViewer.Update();
+
+}
+
+
+  
+  
+
+  
+/*****************************************
+ *
+ *  Clear the seed used to generate the 
+ *  initial level set front
+ *
+ *****************************************/
+void
+GeodesicActiveContour
+::ClearSeeds()
+{
+  this->GeodesicActiveContourBase::ClearSeeds();
+  m_ZeroSetImageViewer.Update();
+  m_InputImageViewer.Update();
+  Fl::check();
+}
+  
+ 
+
+
+/*  Finaly the main() that will instantiate the application  */
+int main()
+{
+
+  try 
+    {
+    GeodesicActiveContour * console = new GeodesicActiveContour();
+    console->ShowConsole();
+    Fl::run();
+    delete console;
+    }
+  catch( itk::ExceptionObject & e )
+    {
+    std::cerr << "ITK exception caught in main" << std::endl;
+    std::cerr << e << std::endl;
+    }
+  catch( std::exception & e )
+    {
+    std::cerr << "STD exception caught in main" << std::endl;
+    std::cerr << e.what() << std::endl;
+    }
+  catch( ... )
+    {
+    std::cerr << "unknown exception caught in main" << std::endl;
+    }
+
+
+  return 0;
+
+}
+
+
+
---
+++ GeodesicActiveContour/GeodesicActiveContourBase.cxx
@@ -0,0 +1,319 @@
+/*=========================================================================
+
+  Program:   Insight Segmentation & Registration Toolkit
+  Module:    GeodesicActiveContourBase.cxx
+  Language:  C++
+  Date:      $Date$
+  Version:   $Revision$
+
+  Copyright (c) 2002 Insight Consortium. All rights reserved.
+  See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even 
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+     PURPOSE.  See the above copyright notices for more information.
+
+=========================================================================*/
+
+#include "GeodesicActiveContourBase.h"
+#include <FL/fl_ask.H>
+
+
+/************************************
+ *
+ *  Constructor
+ *
+ ***********************************/
+GeodesicActiveContourBase 
+::GeodesicActiveContourBase()
+{
+
+  m_ImageReader                  = ImageReaderType::New();
+
+  m_CastImageFilter = CastImageFilterType::New();
+  m_CastImageFilter->SetInput( m_ImageReader->GetOutput() );
+
+  m_DerivativeFilter   = DerivativeFilterType::New();
+  m_DerivativeFilter->SetInput( m_CastImageFilter->GetOutput() );
+
+  m_SigmoidFilter  = SigmoidFilterType::New();
+  m_SigmoidFilter->SetInput( m_DerivativeFilter->GetOutput() );
+  m_SigmoidFilter->SetOutputMaximum( 1.0 );
+  m_SigmoidFilter->SetOutputMinimum( 0.0 );
+
+  m_TrialPoints = NodeContainer::New();
+
+  m_FastMarchingFilter = FastMarchingFilterType::New();
+  m_FastMarchingFilter->SetSpeedConstant( 1.0 );
+  m_FastMarchingFilter->SetTrialPoints( m_TrialPoints );
+
+  m_InputThresholdFilter = ThresholdFilterType::New();
+  m_InputThresholdFilter->SetInput( m_FastMarchingFilter->GetOutput() );
+  m_InputThresholdFilter->SetUpperThreshold( itk::NumericTraits<InternalPixelType>::Zero ); 
+  m_InputThresholdFilter->SetLowerThreshold( itk::NumericTraits<InternalPixelType>::NonpositiveMin() ); 
+  m_InputThresholdFilter->SetInsideValue( 1 );
+  m_InputThresholdFilter->SetOutsideValue(  0 );
+
+  m_EdgeDerivativeFilter = EdgeDerivativeFilterType::New();
+  m_EdgeDerivativeFilter->SetInput( m_SigmoidFilter->GetOutput() );
+  m_EdgeDerivativeFilter->SetSigma( 1.0 );
+
+  m_GeodesicActiveContourFilter = GeodesicActiveContourFilterType::New();
+  m_GeodesicActiveContourFilter->SetInput(  m_FastMarchingFilter->GetOutput() );
+  m_GeodesicActiveContourFilter->SetEdgeImage(   m_SigmoidFilter->GetOutput() );
+  m_GeodesicActiveContourFilter->SetDerivativeImage( m_EdgeDerivativeFilter->GetOutput() );
+
+  m_GeodesicActiveContourFilter->NarrowBandingOn();
+  m_GeodesicActiveContourFilter->SetTimeStepSize( 0.125 );
+
+  m_ThresholdFilter = ThresholdFilterType::New();
+  m_ThresholdFilter->SetInput( m_GeodesicActiveContourFilter->GetOutput() );
+  m_ThresholdFilter->SetUpperThreshold( itk::NumericTraits<InternalPixelType>::Zero ); 
+  m_ThresholdFilter->SetLowerThreshold( itk::NumericTraits<InternalPixelType>::NonpositiveMin() ); 
+  m_ThresholdFilter->SetInsideValue(   1 );
+  m_ThresholdFilter->SetOutsideValue(  0 );
+
+  m_SeedImage = SeedImageType::New();
+
+  m_SeedValue = 0; // It must be set to the minus distance of the initial level set.
+
+  m_NumberOfSeeds = 0;
+
+  m_InputImageIsLoaded  = false;
+
+}
+
+
+
+/************************************
+ *
+ *  Destructor
+ *
+ ***********************************/
+GeodesicActiveContourBase 
+::~GeodesicActiveContourBase()
+{
+
+}
+
+
+
+ 
+/************************************
+ *
+ *  Load Input Image
+ *
+ ***********************************/
+void
+GeodesicActiveContourBase 
+::LoadInputImage( const char * filename )
+{
+  if( !filename )
+  {
+    return;
+  }
+
+  m_ImageReader->SetFileName( filename );
+  m_ImageReader->Update();
+
+  // Allocate a image of seeds of the same size
+  InputImageType::RegionType region 
+            = m_ImageReader->GetOutput()->GetBufferedRegion();
+
+  m_SeedImage->SetRegions( region );
+  m_SeedImage->Allocate();
+  m_SeedImage->FillBuffer( itk::NumericTraits<SeedImageType::PixelType>::Zero );
+
+  m_FastMarchingFilter->SetOutputSize( region.GetSize() );
+
+  m_InputImageIsLoaded = true;
+
+}
+
+
+
+ 
+
+/********************************************
+ *
+ *  Set the seed. From this seed an Initial
+ *  level set image is generated 
+ *
+ *******************************************/
+void
+GeodesicActiveContourBase 
+::AddSeed( const IndexType & seedPosition  )
+{
+
+  // setup trial points
+  NodeType node;
+  
+  node.SetValue( m_SeedValue );
+  node.SetIndex( seedPosition );
+
+  m_TrialPoints->InsertElement( m_NumberOfSeeds, node );
+
+  m_SeedImage->SetPixel( seedPosition, 1 );
+
+  m_NumberOfSeeds++;
+  
+}
+
+
+
+  
+/************************************
+ *
+ *  Clear Seeds
+ *
+ ***********************************/
+void
+GeodesicActiveContourBase 
+::ClearSeeds()
+{
+  m_TrialPoints->Initialize(); 
+  m_NumberOfSeeds = 0;
+  m_FastMarchingFilter->Modified();
+  m_SeedImage->FillBuffer( itk::NumericTraits<SeedImageType::PixelType>::Zero );
+}
+
+
+
+
+/************************************
+ *
+ *  Compute Gradient Magnitude
+ *
+ ***********************************/
+void
+GeodesicActiveContourBase
+::ComputeGradientMagnitude( void )
+{
+  this->ShowStatus("Computing Gradient Image");
+  m_DerivativeFilter->Update();
+}
+
+
+ 
+/************************************
+ *
+ *  Compute Fast Marching
+ *
+ ***********************************/
+void
+GeodesicActiveContourBase
+::ComputeFastMarching( void )
+{
+  this->ShowStatus("Computing Fast Marching");
+  m_FastMarchingFilter->Update();
+}
+
+
+  
+/************************************
+ *
+ *  Compute Zero Set
+ *
+ ***********************************/
+void
+GeodesicActiveContourBase
+::ComputeZeroSet( void )
+{
+  this->ShowStatus("Computing Zero Set");
+  // This will also update the FastMarching
+  m_InputThresholdFilter->Update();
+}
+
+
+ 
+
+
+
+/************************************
+ *
+ *  Compute Edge Potential Image
+ *
+ ***********************************/
+void
+GeodesicActiveContourBase
+::ComputeEdgePotential( void )
+{
+  this->ComputeGradientMagnitude();
+  this->ShowStatus("Computing Edge Potential Image");
+  m_SigmoidFilter->Update();
+}
+
+
+  
+
+/************************************
+ *
+ *  Start Segmentation
+ *
+ ***********************************/
+void
+GeodesicActiveContourBase
+::RunGeodesicActiveContour( void )
+{
+
+  // update the marching filter
+  try
+    {
+    this->ComputeEdgePotential();
+    this->ComputeFastMarching();
+    this->ShowStatus("Computing Geodesic Active Contour Filter");
+    m_GeodesicActiveContourFilter->Update();
+    }
+  catch( itk::ExceptionObject & exp )
+    {
+    fl_alert( exp.GetDescription() );
+    }
+
+}
+
+
+
+
+
+/************************************
+ *
+ *  Stop Segmentation
+ *
+ ***********************************/
+void
+GeodesicActiveContourBase
+::SetZeroSetValue( InternalPixelType value )
+{
+  // By starting the FastMarching front at this value,
+  // the zero set will end up being placed at distance
+  // = value from the seeds. That can be seen as computing
+  // a distance map from the seeds.
+  m_SeedValue = - value;
+
+  NodeContainer::Iterator nodeItr = m_TrialPoints->Begin();
+  NodeContainer::Iterator endNode = m_TrialPoints->End();
+  while( nodeItr != endNode )
+    {
+    nodeItr.Value().SetValue( m_SeedValue );
+    ++nodeItr;
+    }
+  m_FastMarchingFilter->Modified();
+}
+
+
+
+/************************************
+ *
+ *  Stop Segmentation
+ *
+ ***********************************/
+void
+GeodesicActiveContourBase
+::Stop( void )
+{
+  // TODO: add a Stop() method to Filters
+
+}
+
+
+
