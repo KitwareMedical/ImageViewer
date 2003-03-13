502d9bce62ba22bf11681a9edf95f2dd9db5b0d5
Matt Turek <turek@crd.ge.com>
2003-03-13 13:47:48 -0500

    Better png support.  Moved exception handling inside the file loop.

--- DICOMApp/main.cxx
+++ DICOMApp/main.cxx
@@ -19,13 +19,14 @@
 #include "itkPNGImageIO.h"
 #include "itkExceptionObject.h"
 #include "itkRawImageWriter.h"
+#include "itkRescaleIntensityImageFilter.h"
 
 #include <stdlib.h>
 
 
+
 int main(int argc, char* argv[])
 {
-    
   if (argc < 2)
     {
     std::cout << std::endl;
@@ -33,65 +34,80 @@ int main(int argc, char* argv[])
     std::cout << std::endl;
     std::cout << "==================================================================" << std::endl;
     std::cout << "Image data will be written to file1.raw file2.raw and file1.png file2.png" << std::endl;
-    std::cout << "WARNING (to be fixed) - PNG images will not be correct for little endian data." << std::endl;
     std::cout << "Some header values will be written to file1.header.txt file2.header.txt" << std::endl;
     std::cout << "==================================================================" << std::endl;
     std::cout << std::endl;
     std::cout.flush();
     return EXIT_FAILURE;
-    }
-  
+    }  
+
   itk::ObjectFactoryBase::RegisterFactory(itk::DICOMImageIO2Factory::New());
-  
+
   itk::DICOMImageIO2::Pointer DICOMImage = 
     itk::DICOMImageIO2::New();
 
-  typedef itk::Image<unsigned short, 2 > imgType;
-
-  itk::PNGImageIO::Pointer pngThing = itk::PNGImageIO::New();
+  typedef itk::Image<short, 2 > imgType;
+  // typedef itk::Image<itk::RGBPixel<unsigned char>, 2> pngType;
+  typedef itk::Image<unsigned char, 2> pngType;
   
   itk::ImageFileReader<imgType>::Pointer fileReader  = itk::ImageFileReader<imgType>::New();
 
-  itk::ImageFileWriter<imgType>::Pointer pngWriter = itk::ImageFileWriter<imgType>::New();
-  pngWriter->SetImageIO(pngThing);
-  pngWriter->SetInput(fileReader->GetOutput());
+  typedef itk::RescaleIntensityImageFilter< imgType, pngType > RescaleFilterType;
+
+  RescaleFilterType::Pointer rescaler = RescaleFilterType::New();
+  rescaler->SetInput(fileReader->GetOutput());
+  rescaler->SetOutputMinimum( 0 );
+  rescaler->SetOutputMaximum( 255 );
+
+  itk::ImageFileWriter<pngType>::Pointer pngWriter = itk::ImageFileWriter<pngType>::New();
+  pngWriter->SetInput(rescaler->GetOutput());
   
   itk::RawImageWriter<imgType>::Pointer rawWriter = itk::RawImageWriter<imgType>::New();
   rawWriter->SetInput(fileReader->GetOutput());
 
-  try {
+
   for (int i = 1; i < argc; i++)
     {
     const char* filename = argv[i];
+
+    try 
+      {
+
+      std::cout << std::endl;
+      std::cout << "========== Reading " << filename << " ===========" << std::endl;
+      std::cout << std::endl;
+
+      fileReader->SetFileName(filename);
+      fileReader->UpdateLargestPossibleRegion();
     
-    std::cout << std::endl;
-    std::cout << "========== Reading " << filename << " ===========" << std::endl;
-    std::cout << std::endl;
+      std::string rawFilename = std::string(filename) + ".raw";
+      rawWriter->SetFileName(rawFilename.c_str());
+      rawWriter->SetFileTypeToBinary();
+      rawWriter->SetByteOrderToLittleEndian();
+      std::cout << std::endl;
+      std::cout << "Writing: " << rawFilename << std::endl;
+      rawWriter->UpdateLargestPossibleRegion();
+      rawWriter->Write();
+      std::cout << std::endl;
 
-    fileReader->SetFileName(filename);
-    fileReader->UpdateLargestPossibleRegion();
-    
-    std::string rawFilename = std::string(filename) + ".raw";
-    rawWriter->SetFileName(rawFilename.c_str());
-    rawWriter->SetFileTypeToBinary();
-    rawWriter->SetByteOrderToLittleEndian();
-    std::cout << std::endl;
-    std::cout << "Writing: " << rawFilename << std::endl;
-    rawWriter->Write();
-    std::cout << std::endl;
+      std::string outputFilename = std::string(filename) + ".png";
+      pngWriter->SetFileName(outputFilename.c_str());
+      std::cout << std::endl;
+      std::cout << "Writing: " << outputFilename << std::endl;
 
-    std::string outputFilename = std::string(filename) + ".png";
-    pngWriter->SetFileName(outputFilename.c_str());
-    std::cout << std::endl;
-    std::cout << "Writing: " << outputFilename << std::endl;
-
-    pngWriter->Write();
-    }
+      rescaler->UpdateLargestPossibleRegion();
+      pngWriter->UpdateLargestPossibleRegion();
+      pngWriter->Write();
+      }
+    catch (itk::ExceptionObject e)
+      {
+      std::cerr << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
+      std::cerr << " Caught the following exception : " << std::endl;
+      std::cerr << e << std::endl;
+      std::cerr << "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
+      }
   }  
-  catch (itk::ExceptionObject e)
-  {
-    std::cerr << e << std::endl;
-  }
+
   return EXIT_SUCCESS;
 }
 
