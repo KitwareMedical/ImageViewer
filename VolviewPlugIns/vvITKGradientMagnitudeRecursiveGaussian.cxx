4226ce6d37b31b546c742cae1cc2e732c895e328
Luis Ibanez <luis.ibanez@kitware.com>
2003-03-25 17:57:26 -0500

    ENH: Converted to new plugin architecture. Communication is done now using      and API based on strings intead of having direct access to member      variables in the structure.

--- VolviewPlugIns/vvITKGradientMagnitudeRecursiveGaussian.cxx
+++ VolviewPlugIns/vvITKGradientMagnitudeRecursiveGaussian.cxx
@@ -11,6 +11,8 @@ static int ProcessData(void *inf, vtkVVProcessDataStruct *pds)
   vtkVVPluginInfo *info = (vtkVVPluginInfo *)inf;
 
   const unsigned int Dimension = 3;
+ 
+  const float sigma     = atof( info->GetGUIProperty(info, 0, VVP_GUI_VALUE ));
 
   try 
   {
@@ -25,7 +27,7 @@ static int ProcessData(void *inf, vtkVVProcessDataStruct *pds)
       module.SetPlugInfo( info );
       module.SetUpdateMessage("Computing the gradient magnitude...");
       // Set the parameters on it
-      module.GetFilter()->SetSigma(  atof( info->GUIItems[ 0 ].CurrentValue) );
+      module.GetFilter()->SetSigma( sigma ); 
       module.GetFilter()->SetNormalizeAcrossScale( true );
       // Execute the filter
       module.ProcessData( pds  );
@@ -40,7 +42,7 @@ static int ProcessData(void *inf, vtkVVProcessDataStruct *pds)
       module.SetPlugInfo( info );
       module.SetUpdateMessage("Computing the gradient magnitude...");
       // Set the parameters on it
-      module.GetFilter()->SetSigma(  atof( info->GUIItems[ 0 ].CurrentValue) );
+      module.GetFilter()->SetSigma( sigma ); 
       module.GetFilter()->SetNormalizeAcrossScale( true );
       // Execute the filter
       module.ProcessData( pds );
@@ -50,7 +52,7 @@ static int ProcessData(void *inf, vtkVVProcessDataStruct *pds)
   }
   catch( itk::ExceptionObject & except )
   {
-    info->DisplayError( info, except.what() ); 
+    info->SetProperty( info, VVP_ERROR, except.what() ); 
     return -1;
   }
   return 0;
@@ -61,14 +63,14 @@ static int UpdateGUI(void *inf)
 {
   vtkVVPluginInfo *info = (vtkVVPluginInfo *)inf;
 
-  info->GUIItems[0].Label = "Sigma";
-  info->GUIItems[0].GUIType = VV_GUI_SCALE;
-  info->GUIItems[0].Default = "1.0";
-  info->GUIItems[0].Help = "Standard deviation of the Gaussian kernel used to smooth the image before computing the gradient";
-  info->GUIItems[0].Hints = "0 20 0.1";
+  info->SetGUIProperty(info, 0, VVP_GUI_LABEL, "Sigma");
+  info->SetGUIProperty(info, 0, VVP_GUI_TYPE, VVP_GUI_SCALE);
+  info->SetGUIProperty(info, 0, VVP_GUI_DEFAULT, "1,0");
+  info->SetGUIProperty(info, 0, VVP_GUI_HELP, "Standard deviation of the Gaussian kernel used to smooth the image before computing the gradient");
+  info->SetGUIProperty(info, 0, VVP_GUI_HINTS , "0 20 0.1");
 
-  info->RequiredZOverlap = 0;
-  
+  info->SetProperty(info, VVP_REQUIRED_Z_OVERLAP, "0");
+
   info->OutputVolumeScalarType = info->InputVolumeScalarType;
   info->OutputVolumeNumberOfComponents = 
     info->InputVolumeNumberOfComponents;
@@ -90,20 +92,17 @@ void VV_PLUGIN_EXPORT vvITKGradientMagnitudeRecursiveGaussianInit(vtkVVPluginInf
   // setup information that never changes
   info->ProcessData = ProcessData;
   info->UpdateGUI = UpdateGUI;
-  info->Name = "Gradient Magnitude IIR (ITK)";
-  info->TerseDocumentation = "Gradient Magnitude Gaussian IIR";
-  info->FullDocumentation = 
-    "This filter applies IIR filters to compute the equivalent of convolving the input image with the derivatives of a Gaussian kernel and then computing the magnitude of the resulting gradient.";
-  info->SupportsInPlaceProcessing = 0;
-  info->SupportsProcessingPieces = 0;
-  info->RequiredZOverlap = 0;
+  info->SetProperty(info, VVP_NAME, "Gradient Magnitude IIR (ITK)");
+  info->SetProperty(info, VVP_TERSE_DOCUMENTATION,
+                                "Gradient Magnitude Gaussian IIR");
+  info->SetProperty(info, VVP_FULL_DOCUMENTATION,
+    "This filter applies IIR filters to compute the equivalent of convolving the input image with the derivatives of a Gaussian kernel and then computing the magnitude of the resulting gradient.");
 
-  // Number of bytes required in intermediate memory per voxel
-  info->PerVoxelMemoryRequired = 16; 
-  
-  /* setup the GUI components */
-  info->NumberOfGUIItems = 1;
-  info->GUIItems = (vtkVVGUIItem *)malloc(info->NumberOfGUIItems*sizeof(vtkVVGUIItem));
+  info->SetProperty(info, VVP_SUPPORTS_IN_PLACE_PROCESSING, "0");
+  info->SetProperty(info, VVP_SUPPORTS_PROCESSING_PIECES,   "0");
+  info->SetProperty(info, VVP_NUMBER_OF_GUI_ITEMS,          "1");
+  info->SetProperty(info, VVP_REQUIRED_Z_OVERLAP,           "0");
+  info->SetProperty(info, VVP_PER_VOXEL_MEMORY_REQUIRED,    "8"); 
 }
 
 }
