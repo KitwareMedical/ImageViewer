b62e7dd0e1727b25bcf21efafcc934ea63f8a8a4
Bill Lorensen <bill.lorensen@gmail.com>
2012-01-25 15:51:38 -0500

    (split) ENH: Patch FLTK's CMakeList file.
    Some gcc builds need -ldl to link fltk.a. This patch uses the
    ExternalProject patch command to patch FLTK's CMakeList file.
    
    Change-Id: If7ac47caa69a69fe203cc2521771b798615995ac

--- External-FLTK.cmake
+++ External-FLTK.cmake
@@ -2,23 +2,31 @@
 # Get and build fltk
 #
 #-----------------------------------------------------------------------------
-set(FLTK_INSTALL_COMMAND ${CMAKE_MAKE_COMMAND} install)
-
-if(CMAKE_COMPILER_IS_GNUCXX)
-  set(EXTRA_LIBRARIES "-DCMAKE_EXE_LINKER_FLAGS:STRING=-ldl")
+if( NOT WIN32)
+  configure_file(
+    ${CMAKE_SOURCE_DIR}/patch-FLTK.sh.in
+    ${CMAKE_BINARY_DIR}/patch-FLTK.sh
+    @only
+  )
+  set(FLTK_PATCH_COMMAND "${CMAKE_BINARY_DIR}/patch-FLTK.sh")
+else ()
+  set(FLTK_PATCH_COMMAND "")
 endif()
+
+set(FLTK_INSTALL_COMMAND ${CMAKE_MAKE_COMMAND} install)
 
 ExternalProject_Add(FLTK
   SVN_REPOSITORY "http://svn.easysw.com/public/fltk/fltk/branches/branch-1.3"
   SVN_REVISION ""
   UPDATE_COMMAND ""
+  PATCH_COMMAND ${FLTK_PATCH_COMMAND}
   SOURCE_DIR FLTK
   BINARY_DIR FLTK-build
   CMAKE_GENERATOR ${gen}
   CMAKE_ARGS
     ${ep_common_args}
     -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_BINARY_DIR}/FLTK-install
-    ${EXTRA_LIBRARIES}
 )
+
 set(FLTK_DIR ${CMAKE_BINARY_DIR}/FLTK-install/lib/FLTK-1.3)
 set(FLUID_COMMAND ${CMAKE_BINARY_DIR}/FLTK-install/bin/fluid)
