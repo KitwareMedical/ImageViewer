3752c5c5575d7a3899e80fe28d3f0f489adf0fad
Bill Lorensen <bill.lorensen@gmail.com>
2012-07-17 08:19:35 -0400

    (split) ENH: ITK legacy and compatibility.
    ITKApps needs ITK to be configured with ITK_LEGACY_REMOVE OFF and
    ITKV3_COMPATIBILITY ON.
    
    Change-Id: I2f19bc5e4c9f0581c79b90fe41a60cdd7101cdb7

--- External-ITK.cmake
+++ External-ITK.cmake
@@ -2,7 +2,7 @@
 # Get and build itk
 
 set( ITK_TAG "v4.2.0" )
-
+set(ITK_TAG "2a7eed39f0a26f5eb9d0d9ec413938e9f0b9995c") # July 17, 2012
 ExternalProject_Add( ITK
   GIT_REPOSITORY "${git_protocol}://itk.org/ITK.git"
   GIT_TAG "${ITK_TAG}"
@@ -15,7 +15,8 @@ ExternalProject_Add( ITK
     -DBUILD_EXAMPLES:BOOL=OFF
     -DBUILD_TESTING:BOOL=OFF
     -DITK_BUILD_ALL_MODULES:BOOL=ON
-    -DITK_LEGACY_REMOVE:BOOL=ON
+    -DITK_LEGACY_REMOVE:BOOL=OFF
+    -DITKV3_COMPATIBILITY:BOOL=ON
     -DVTK_DIR:PATH=${VTK_DIR}
     -DModule_ITKVtkGlue:BOOL=ON
   INSTALL_COMMAND ""
