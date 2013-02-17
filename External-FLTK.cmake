a5bbe4b9e7254b90a4ff1dcd5f23ff87e75b12f2
Matt McCormick <matt.mccormick@kitware.com>
2013-02-17 13:44:44 -0500

    (split) COMP: Re-add the FLTK patch for some Macs.
    Some Macs still need the FLTK patch.
    
    Change-Id: I95b3659c5bc75501277cf1d051772d1e7a3a64a5
    Suggested-by: Bill Lorensen <bill.lorensen@gmail.com>

--- External-FLTK.cmake
+++ External-FLTK.cmake
@@ -2,12 +2,24 @@
 # Get and build fltk
 #
 #-----------------------------------------------------------------------------
+if( NOT WIN32)
+  configure_file(
+    ${CMAKE_SOURCE_DIR}/patch-FLTK.sh.in
+    ${CMAKE_BINARY_DIR}/patch-FLTK.sh
+    @only
+  )
+  set(FLTK_PATCH_COMMAND "${CMAKE_BINARY_DIR}/patch-FLTK.sh")
+else ()
+  set(FLTK_PATCH_COMMAND "")
+endif()
+
 set(FLTK_INSTALL_COMMAND ${CMAKE_MAKE_COMMAND} install)
 
 ExternalProject_Add(FLTK
   SVN_REPOSITORY "http://svn.easysw.com/public/fltk/fltk/branches/branch-1.3"
   SVN_REVISION -r "9815"
   UPDATE_COMMAND ""
+  PATCH_COMMAND ${FLTK_PATCH_COMMAND}
   SOURCE_DIR FLTK
   BINARY_DIR FLTK-build
   CMAKE_GENERATOR ${gen}
