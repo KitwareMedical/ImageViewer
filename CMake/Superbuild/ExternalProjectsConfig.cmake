f3c5f596b49aab3a3e6a448646bb2d57f0c9794f
Stephen R. Aylward <stephen.aylward@kitware.com>
2014-07-13 22:55:37 -0400

    ENH: update tags of external projects to match version used by Slicer

--- CMake/Superbuild/ExternalProjectsConfig.cmake
+++ CMake/Superbuild/ExternalProjectsConfig.cmake
@@ -25,14 +25,14 @@ set( FLTK_URL http://midas3.kitware.com/midas/download/item/208771/fltk-1.3.2.20
 set( FLTK_URL_MD5_TAG f8b2e249701cf77520546e3c37130f0b )
 
 # Insight Segmentation and Registration Toolkit
-set( ITK_URL ${github_protocol}://github.com/Kitware/ITK.git )
-set( ITK_URL_OR_TAG 9fb84edbf9f6a5bb0846acbf6ecd767ca5cab2e1 )
+set( ITK_URL ${github_protocol}://github.com/Slicer/ITK.git )
+set( ITK_URL_OR_TAG 3307a27de52d921ab40573e77097bfc72d70f5d6 )
 
 set( CTK_URL https://github.com/commontk/CTK.git )
-set( CTK_URL_MD5_TAG 0e21d85e7b9187c36c66d5934041323eff0685c2 )
+set( CTK_URL_MD5_TAG 282abc745a7db700a822822375d0b8c170ec1a56 )
 
 set( VTK_URL https://github.com/Slicer/VTK.git )
-set( VTK_URL_MD5_TAG 6da2c7235b8de9d8663a62f3246198d6865ecfae )
+set( VTK_URL_MD5_TAG d8540e7ee356bbc025cb9917a41b7c7fa0548d4b )
 
 # JsonCpp snapshot 2014-04-15 r276
 set( JsonCpp_URL http://midas3.kitware.com/midas/download/bitstream/366544/JsonCpp_r276.tar.gz )
@@ -41,9 +41,9 @@ set( JsonCpp_HASH_OR_TAG 192f0cf2b00798d4f4fb29c99a3aa83c )
 # TubeTK Parameter Serializer snapshot 2014-02-05 )
 set( ParameterSerializer_URL
   https://github.com/Slicer/ParameterSerializer.git )
-set( ParameterSerializer_HASH_OR_TAG df985d8f7e7751a10852ca53b2b38c0fa1620a99 )
+set( ParameterSerializer_HASH_OR_TAG d7e94d3ff9de9a8782db6997f8e1bc45626cfa3b )
 
 # Slicer Execution Model snapshot 2014-02-18
 set( SlicerExecutionModel_URL
   ${github_protocol}://github.com/Slicer/SlicerExecutionModel.git )
-set( SlicerExecutionModel_HASH_OR_TAG 865e2a771a47c2a3daa3bd8b9a6fc6bac7537255 )
+set( SlicerExecutionModel_HASH_OR_TAG ecb58ad73c5dcdebf8c4d46257c7e44a07a7cdd4 )
