94c2f4c6622362725a578aa8fcca86f03a2114e5
Stephen Aylward <stephen.aylward@kitware.com>
2023-09-24 15:47:23 -0400

    BUG: Fix path for mac qt

--- CMake/deployqt.cmake
+++ CMake/deployqt.cmake
@@ -24,7 +24,7 @@ elseif( APPLE )
   function(deployqt target)
     add_custom_command(TARGET ${target} POST_BUILD
       COMMAND "${_qt_bin_dir}/macdeployqt"         
-              \"$<TARGET_FILE:${target}>\"
+              \"$<TARGET_FILE_DIR:${target}>/../..\"
       COMMENT "Deploying using macdeployqt for target '${target}' ..."
     )
     add_custom_command(TARGET ${target} POST_BUILD
