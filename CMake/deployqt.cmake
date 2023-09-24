55f5a61a912cee3a5662dc7be5ef46af5dd87077
Stephen Aylward <stephen.aylward@kitware.com>
2023-09-24 09:11:56 -0400

    ENH:Run windeployqt/macdeployqt as part of build process
    Ensure compilation performs all steps necessary for ImageViewer to be
    executated on Window and Macs.

---
+++ CMake/deployqt.cmake
@@ -0,0 +1,45 @@
+# Copied from https://stackoverflow.com/questions/60854495/qt5-cmake-include-all-libraries-into-executable
+
+find_package(Qt5Core REQUIRED)
+
+# get absolute path to qmake, then use it to find windeployqt executable
+get_target_property(_qmake_executable Qt5::qmake IMPORTED_LOCATION)
+get_filename_component(_qt_bin_dir "${_qmake_executable}" DIRECTORY)
+
+if( WIN32 )
+  function(deployqt target)
+    add_custom_command(TARGET ${target} POST_BUILD
+      COMMAND "${_qt_bin_dir}/windeployqt.exe"         
+              --verbose 1
+              --translations en
+              --release
+              --no-svg
+              --no-angle
+              --no-system-d3d-compiler
+              \"$<TARGET_FILE:${target}>\"
+      COMMENT "Deploying using windeployqt for target '${target}' ..."
+    )
+  endfunction()
+endif(WIN32)
+
+if( APPLE )
+  function(deployqt target)
+    add_custom_command(TARGET ${target} POST_BUILD
+      COMMAND "${_qt_bin_dir}/macdeployqt.exe"         
+              --verbose 1
+              --translations en
+              --release
+              --no-svg
+              --no-angle
+              --no-system-d3d-compiler
+              \"$<TARGET_FILE:${target}>\"
+      COMMENT "Deploying using macdeployqt for target '${target}' ..."
+    )
+    add_custom_command(TARGET ${target} POST_BUILD
+      COMMAND "chmod"
+                 +x
+                 \"$<TARGET_FILE:${target}>/Contents/MacOS/ImageViewer\"
+      COMMENT "Ensuring ImageViewer application is executable."
+    )
+  endfunction()
+endif(APPLE)
